749302269bcf944fba582ceab12b39bd29c1d4b0
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-06-10 05:36:57 +0300

    Tag/value lexer tests
    Signed-off-by: Ahmed H. Ismail <ahm3d.hisham@gmail.com>

--- tests/tag_value_parser_tests.py
+++ tests/tag_value_parser_tests.py
@@ -14,5 +14,75 @@
 import nose
 from spdx.parsers.tagvalue import Lexer
 
-class LexerTests(object):
-    pass
+class TestLexer(object):
+    def __init__(self):
+        self.l = Lexer()
+        self.l.build()
+
+    def test_document(self):
+        data = '''
+        SPDXVersion: SPDX-1.1
+        # Comment.
+        DataLicense: CC0-1.0
+        DocumentComment: <text>This is a sample spreadsheet</text>
+        '''
+        self.l.read(data)
+        self.token_assert_helper(self.l.token(), 'DOC_VERSION', 'SPDXVersion', 2)
+        self.token_assert_helper(self.l.token(), 'LINE', 'SPDX-1.1', 2)
+        self.token_assert_helper(self.l.token(), 'DOC_LICENSE', 'DataLicense', 4)
+        self.token_assert_helper(self.l.token(), 'LINE', 'CC0-1.0', 4)
+        self.token_assert_helper(self.l.token(), 'DOC_COMMENT', 'DocumentComment', 5)
+        self.token_assert_helper(self.l.token(), 'TEXT', 
+            '<text>This is a sample spreadsheet</text>',5)
+
+    def test_creation_info(self):
+        data = '''
+        ## Creation Information
+        Creator: Person: Gary O'Neall
+        Creator: Organization: Source Auditor Inc.
+        Creator: Tool: SourceAuditor-V1.2
+        Created: 2010-02-03T00:00:00Z
+        CreatorComment: <text>This is an example of an SPDX spreadsheet format</text>
+        '''
+        self.l.read(data)
+        self.token_assert_helper(self.l.token(), 'CREATOR', 'Creator', 3)
+        self.token_assert_helper(self.l.token(), 'PERSON_VALUE', "Person: Gary O'Neall",
+            3)
+        self.token_assert_helper(self.l.token(), 'CREATOR', 'Creator', 4)
+        self.token_assert_helper(self.l.token(), 'ORG_VALUE',
+            'Organization: Source Auditor Inc.', 4)
+        self.token_assert_helper(self.l.token(), 'CREATOR', 'Creator', 5)
+        self.token_assert_helper(self.l.token(), 'TOOL_VALUE',
+            'Tool: SourceAuditor-V1.2', 5)
+        self.token_assert_helper(self.l.token(), 'CREATED', 'Created', 6)
+        self.token_assert_helper(self.l.token(), 'DATE', 
+           '2010-02-03T00:00:00Z', 6)
+
+    def test_review_info(self):
+        data = '''
+        Reviewer: Person: Joe Reviewer
+        ReviewDate: 2010-02-10T00:00:00Z
+        ReviewComment: <text>This is just an example.  
+        Some of the non-standard licenses look like they are actually 
+        BSD 3 clause licenses</text>
+        '''
+        self.l.read(data)
+        self.token_assert_helper(self.l.token(), 'REVIEWER', 'Reviewer', 2)
+        self.token_assert_helper(self.l.token(), 'PERSON_VALUE', 
+            "Person: Joe Reviewer", 2)
+        self.token_assert_helper(self.l.token(), 'REVIEW_DATE', 'ReviewDate', 3)
+        self.token_assert_helper(self.l.token(), 'DATE', 
+            '2010-02-10T00:00:00Z', 3)
+        self.token_assert_helper(self.l.token(), 'REVIEW_COMMENT', 
+            'ReviewComment', 4)
+        self.token_assert_helper(self.l.token(), 'TEXT', 
+            '''<text>This is just an example.  
+        Some of the non-standard licenses look like they are actually 
+        BSD 3 clause licenses</text>''', 4)
+        
+    def token_assert_helper(self, token, type, value, line):
+        print token
+        assert token.type == type
+        assert token.value == value
+        assert token.lineno == line
+
