6d9fb44a76430cd4637953a6d61ec1a9513b1ecc
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-06-23 16:19:56 +0300

    Creates file module
    Signed-off-by: Ahmed H. Ismail <ahm3d.hisham@gmail.com>

---
+++ spdx/file.py
@@ -0,0 +1,22 @@
+# Copyright 2014 Ahmed H. Ismail
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+class File(object):
+    """Representation of SPDX file.
+    Fields:
+    name - File name, str mandatory one.
+    """
+    def __init__(self, name):
+        super(File, self).__init__()
+        self.name = name
