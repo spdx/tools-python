cf73e536392c0f807d83ee7cfc0e05154a3b12d8
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-06-26 05:55:57 +0300

    Adds LicenseLise parser and lexer
    Signed-off-by: Ahmed H. Ismail <ahm3d.hisham@gmail.com>

--- spdx/utils.py
+++ spdx/utils.py
@@ -13,6 +13,8 @@
 #    limitations under the License.
 import re
 import datetime
+import document
+from ply import yacc, lex
 
 
 def datetime_iso_format(date):
@@ -46,3 +48,109 @@ def datetime_from_iso_format(string):
 
 class NoAssert(object):
     pass
+
+
+class LicenseListLexer(object):
+
+    def __init__(self):
+        super(LicenseListLexer, self).__init__()
+
+    tokens = ['LP', 'RP', 'AND', 'OR', 'LICENSE']
+
+    def t_LP(self, t):
+        r'\('
+        return t
+
+    def t_RP(self, t):
+        r'\)'
+        return t
+
+    def t_AND(self, t):
+        r'\sand\s'
+        t.value = t.value.strip()
+        return t
+
+    def t_OR(self, t):
+        r'\sor\s'
+        t.value = t.value.strip()
+        return t
+
+    def t_whitespace(self, t):
+        r'\s+'
+        pass
+
+    def t_LICENSE(self, t):
+        r'[A-Za-z.0-9\-]+'
+        t.value = t.value.strip()
+        return t
+
+    def t_error(self, t):
+        pass
+
+    def input(self, data):
+        self.lexer.input(data)
+
+    def token(self):
+        return self.lexer.token()
+
+    def build(self, **kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
+
+
+class LicenseListParser(object):
+
+    def __init__(self):
+        super(LicenseListParser, self).__init__()
+        self.lex = LicenseListLexer()
+        self.lex.build(reflags=re.UNICODE)
+        self.tokens = self.lex.tokens
+
+    def p_license_list_1(self, p):
+        """license_list : LP conjuctions RP 
+                        | LP disjunctions RP
+        """
+        p[0] = p[2]
+
+    def p_license_list_2(self, p):
+        """license_list : error"""
+        p[0] = None
+
+    def p_conjuctions_1(self, p):
+        """conjuctions : conjuctions conjuction"""
+        p[0] = document.LicenseConjuction(p[1], p[2])
+
+    def p_conjuctions_2(self, p):
+        """conjuctions : conjuction"""
+        p[0] = p[1]
+
+    def p_disjunctions_1(self, p):
+        """disjunctions : disjunctions disjunction"""
+        p[0] = document.LicenseDisjunction(p[1], p[2])
+
+    def p_disjunctions_2(self, p):
+        """disjunctions : disjunction"""
+        p[0] = p[1]
+
+    def p_conjuction(self, p):
+        """conjuction : license AND license"""
+        p[0] = document.LicenseConjuction(p[1], p[3])
+
+    def p_disjunction(self, p):
+        """disjunction : license OR license"""
+        p[0] = document.LicenseDisjunction(p[1], p[3])
+
+    def p_license(self, p):
+        """license : LICENSE"""
+        p[0] = document.License.from_identifier(p[1])
+
+    def p_error(self, p):
+        pass
+
+    def build(self, **kwargs):
+        self.yacc = yacc.yacc(module=self, **kwargs)
+
+    def parse(self, data):
+        try:
+            return self.yacc.parse(data, lexer=self.lex)
+        except:
+            return None
