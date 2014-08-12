ccfd2c742c4c3423808998ef6064132e48057d8b
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-08-12 08:54:24 +0300

    Implements spdx.writers.rdf module: 	Creates entry point function write_document 	Implements BaseWriter class which contains utility functions and shared fields. 	Implements LicenseWriter class which handles writing license information, 	used by other writers to handle license fields. 	Implements FileWriter class which handles writing spdx.file models. 	Implements ReviewInfoWriter class which handles writing document review information. 	Implements CreationInfoWriter class which handles writing document creation info. 	Implements PackageWriter class which handles writing spdx.document model. 	Implements WriterClass which utilizes all other writers to write the entire document.
    Adds rdf pretty print example file rdf_pp.py.
    Adds tests for conversion from tag/value to rdf and rdf to tag/value.
    Updates README files.
    Signed-off-by: Ahmed H. Ismail <ahm3d.hisham@gmail.com>

---
+++ rdf_pp.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+if __name__ == '__main__':
+    import sys
+    import spdx.file as spdxfile
+    from spdx.parsers.rdf import Parser
+    from spdx.parsers.loggers import StandardLogger
+    from spdx.parsers.rdfbuilders import Builder
+    from spdx.writers.rdf import write_document
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+    p = Parser(Builder(), StandardLogger())
+    with open(infile) as f:
+        doc, error = p.parse(f)
+        if not error:
+            with open(outfile, mode='w') as out:
+                write_document(doc, out)
+        
+        else:
+            print 'Errors while parsing'
