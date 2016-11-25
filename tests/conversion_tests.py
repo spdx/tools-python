7e32b95bed1527453a622af1a458adb86013e176
Philippe Ombredanne <pombredanne@nexb.com>
2016-11-25 07:21:41 -0800

    Do not create temp test files at the root
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/conversion_tests.py
+++ tests/conversion_tests.py
@@ -1,20 +1,24 @@
-# Copyright 2014 Ahmed H. Ismail
+# Copyright (c) 2014 Ahmed H. Ismail
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
+from __future__ import absolute_import
+from __future__ import print_function
 
-#        http://www.apache.org/licenses/LICENSE-2.0
-
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
-import nose
 import codecs
-import random
+import os
 import string
+import tempfile
+
+import nose
+
 import spdx.writers.tagvalue as tvwriter
 from spdx.parsers.tagvalue import Parser as TVParser
 from spdx.parsers.tagvaluebuilders import Builder as TVBuilder
@@ -24,13 +28,21 @@ from spdx.parsers.rdfbuilders import Builder as RDFBuilder
 from spdx.parsers.loggers import StandardLogger
 
 
+def get_temp_file(extension=''):
+    """
+    Return a unique new temporary file location to a non-existing
+    temporary file that can safely be created without a risk of name
+    collision.
+    """
+
+    if extension and not extension.startswith('.'):
+            extension = '.' + extension
+    file_name = 'temp_file' + extension
+    temp_dir = tempfile.mkdtemp()
+    return os.path.join(temp_dir, file_name)
 
 
 class TestConversions(object):
-
-    def random_file(self, length, suffix):
-       rand_string =  ''.join(random.choice(string.uppercase) for _ in xrange(length))
-       return rand_string + suffix
 
     def parse_rdf_file(self, file_name):
         """Returns tuple error, document."""
@@ -38,9 +50,8 @@ class TestConversions(object):
             rdfparser = RDFParser(RDFBuilder(), StandardLogger())
             return rdfparser.parse(infile)
 
-
     def parse_tagvalue_file(self, file_name):
-        """Returns tuple error, document."""    
+        """Returns tuple error, document."""
         with open(file_name, mode='r') as infile:
             tvparser = TVParser(TVBuilder(), StandardLogger())
             tvparser.build()
@@ -50,7 +61,6 @@ class TestConversions(object):
         with codecs.open(file_name, mode='w', encoding='utf-8') as out:
             tvwriter.write_document(document, out)
 
-
     def write_rdf_file(self, document, file_name):
         with open(file_name, mode='w') as out:
             rdfwriter.write_document(document, out)
@@ -59,7 +69,7 @@ class TestConversions(object):
         doc, error = self.parse_tagvalue_file('data/SPDXTagExample.tag')
         assert not error
         assert doc.validate([])
-        filename = self.random_file(10, '.rdf')
+        filename = get_temp_file('.rdf')
         self.write_rdf_file(doc, filename)
         doc, error = self.parse_rdf_file(filename)
         assert not error
@@ -69,17 +79,17 @@ class TestConversions(object):
         doc, error = self.parse_rdf_file('data/SPDXRdfExample.rdf')
         assert not error
         assert doc.validate([])
-        filename = self.random_file(10, '.rdf')
+        filename = get_temp_file('.rdf')
         self.write_rdf_file(doc, filename)
         doc, error = self.parse_rdf_file(filename)
         assert not error
-        assert doc.validate([])  
+        assert doc.validate([])
 
     def test_tagvalue_tagvalue(self):
         doc, error = self.parse_tagvalue_file('data/SPDXTagExample.tag')
         assert not error
         assert doc.validate([])
-        filename = self.random_file(10, '.tag')
+        filename = get_temp_file('.tag')
         self.write_tagvalue_file(doc, filename)
         doc, error = self.parse_tagvalue_file(filename)
         assert not error
@@ -89,7 +99,7 @@ class TestConversions(object):
         doc, error = self.parse_rdf_file('data/SPDXRdfExample.rdf')
         assert not error
         assert doc.validate([])
-        filename = self.random_file(10, '.tag')
+        filename = get_temp_file('.tag')
         self.write_tagvalue_file(doc, filename)
         doc, error = self.parse_tagvalue_file(filename)
         assert not error
