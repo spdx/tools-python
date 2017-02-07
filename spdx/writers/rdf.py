c86b526246e8c12f27ff95eb0451114c2faee051
Hugo Jacob <Hugo.Jacob@bmw.de>
2017-02-07 11:43:55 +0100

    writers/rdf: Fix the license node type decision tree

--- spdx/writers/rdf.py
+++ spdx/writers/rdf.py
@@ -152,9 +152,9 @@ class LicenseWriter(BaseWriter):
         Could be a single license (extracted or part of license list.) or
         a conjunction/disjunction of licenses.
         """
-        if isinstance(license, document.LicenseConjunction):
+        if isinstance(lic, document.LicenseConjunction):
             return self.create_conjunction_node(lic)
-        elif isinstance(license, document.LicenseDisjunction):
+        elif isinstance(lic, document.LicenseDisjunction):
             return self.create_disjunction_node(lic)
         else:
             return self.create_license_helper(lic)
