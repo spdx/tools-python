65955b283a9f481ca78ca4a77078f472e7d2578b
yash-nisar <yash.nisar@somaiya.edu>
2018-07-13 16:00:57 +0800

    Report correct line no.s while showing errors #77
    This occurs due to the method `t_text_end` in the
    file `lexers\tagvalue.py`. The line `t.value.count('\n')`
    doesn't detect newlines because the earlier line is stripped
    of whitespaces and newlines. This issue is resolved by first
    counting the no. of newline chars and then stripping
    whitespaces and newlines so information is preserved.
    
    Signed-off-by: Yash Nisar <yash.nisar@somaiya.edu>

--- spdx/parsers/lexers/tagvalue.py
+++ spdx/parsers/lexers/tagvalue.py
@@ -94,8 +94,9 @@ class Lexer(object):
         r'</text>\s*'
         t.type = 'TEXT'
         t.value = t.lexer.lexdata[
-            t.lexer.text_start:t.lexer.lexpos].strip()
+            t.lexer.text_start:t.lexer.lexpos]
         t.lexer.lineno += t.value.count('\n')
+        t.value = t.value.strip()
         t.lexer.begin('INITIAL')
         return t
 
