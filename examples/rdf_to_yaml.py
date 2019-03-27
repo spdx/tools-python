b53e5c79233eb9cd10387839aa98b0d64583caf2
Xavier Figueroa <xavierfigueroav@gmail.com>
2019-03-27 17:02:43 -0500

    Add examples that convert from RDF and tag/value formats to JSON and YAML.
    Signed-off-by: Xavier Figueroa <xavierfigueroav@gmail.com>

---
+++ examples/rdf_to_yaml.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+if __name__ == '__main__':
+    import sys
+    from spdx.parsers.rdf import Parser
+    from spdx.parsers.loggers import StandardLogger
+    from spdx.parsers.rdfbuilders import Builder
+    from spdx.writers.yaml import write_document
+
+    file = sys.argv[1]
+    p = Parser(Builder(), StandardLogger())
+    with open(file) as f:
+        document, error = p.parse(f)
+        
+        if not error:
+            with open('yaml_from_rdf_example.yml', 'w') as out:
+                write_document(document, out)
+        else:
+            print 'Errors encountered while parsing'
