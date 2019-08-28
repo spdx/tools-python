a0f25d6af6f5b928bfdbb4f004cc04fb42c736a2
Philippe Ombredanne <pombredanne@nexb.com>
2019-08-28 12:03:21 +0200

    Use proper regex escaping
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- spdx/parsers/tagvaluebuilders.py
+++ spdx/parsers/tagvaluebuilders.py
@@ -42,7 +42,7 @@ def checksum_from_sha1(value):
     checksum or None if does not match CHECKSUM_RE.
     """
     # More constrained regex at lexer level
-    CHECKSUM_RE = re.compile('SHA1:\s*([\S]+)', re.UNICODE)
+    CHECKSUM_RE = re.compile('SHA1:\\s*([\\S]+)', re.UNICODE)
     match = CHECKSUM_RE.match(value)
     if match:
         return checksum.Algorithm(identifier='SHA1', value=match.group(1))
