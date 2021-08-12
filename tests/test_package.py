da3d5c95e3eb4e219683be5db9f83d40962c50a4
Nathan Voss <nathan@finitestate.io>
2021-08-12 09:57:03 -0700

    Added test for non-SHA1 checksum on package
    Signed-off-by: Nathan Voss <nathan@finitestate.io>

--- tests/test_package.py
+++ tests/test_package.py
@@ -12,6 +12,7 @@
 import unittest
 from unittest import TestCase
 
+from spdx.checksum import Algorithm
 from spdx.package import Package
 
 
@@ -20,6 +21,14 @@ class TestPackage(TestCase):
     def test_calc_verif_code(self):
         package = Package()
         package.calc_verif_code()
+
+    def test_package_with_non_sha1_check_sum(self):
+        package = Package()
+        package.check_sum = Algorithm("SHA256", '')
+
+        # Make sure that validation still works despite the checksum not being SHA1
+        messages = []
+        messages = package.validate_checksum(messages)
 
 
 if __name__ == '__main__':
