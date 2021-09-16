52db7ebc00fa4359a31489481d2d9d2bf4e284fe
Philippe Ombredanne <pombredanne@nexb.com>
2021-09-16 14:49:47 +0200

    Bump version
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='spdx-tools',
-    version='0.6.1',
+    version='0.7.0a1',
     description='SPDX parser and tools.',
     long_description=long_description,
     long_description_content_type='text/markdown',
