b7068ae254ba0793bbc2c10b44d527c493590872
Meret Behrens <meret.behrens@tngtech.com>
2022-10-21 11:40:18 +0200

    [issue-246] delete namespace from File_SPDXID
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- spdx/parsers/rdfbuilders.py
+++ spdx/parsers/rdfbuilders.py
@@ -467,6 +467,27 @@ class FileBuilder(tagvaluebuilders.FileBuilder):
         else:
             raise OrderError("File::Notice")
 
+    def set_file_spdx_id(self, doc, spdx_id):
+        """
+        Set the file SPDX Identifier.
+        Raise OrderError if no package or no file defined.
+        Raise SPDXValueError if malformed value.
+        Raise CardinalityError if more than one spdx_id set.
+        """
+        if self.has_package(doc) and self.has_file(doc):
+            if not self.file_spdx_id_set:
+                self.file_spdx_id_set = True
+                spdx_id = spdx_id.split("#")[-1]
+                if validations.validate_file_spdx_id(spdx_id):
+                    self.file(doc).spdx_id = spdx_id
+                    return True
+                else:
+                    raise SPDXValueError("File::SPDXID")
+            else:
+                raise CardinalityError("File::SPDXID")
+        else:
+            raise OrderError("File::SPDXID")
+
 
 class SnippetBuilder(tagvaluebuilders.SnippetBuilder):
     def __init__(self):
