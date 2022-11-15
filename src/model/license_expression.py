5c5ee6d34ee2fd66dd67c8b75ec082511ed0efb2
Nicolaus Weidner <nicolaus.weidner@tngtech.com>
2022-11-15 13:13:11 +0100

    [issue-290] Add refactored file data structure
    Signed-off-by: Nicolaus Weidner <nicolaus.weidner@tngtech.com>

---
+++ src/model/license_expression.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2022 spdx contributors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class LicenseExpression:
+    """So far, this just holds a string with the license expression. The ticket for adding license expression support
+    is https://github.com/spdx/tools-python/issues/10."""
+    expression_string: str
---
+++ src/model/spdx_none.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2022 spdx contributors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class SpdxNone:
+    """
+    Represents the SPDX NONE value.
+    """
+
+    _string_value = "NONE"
+
+    def __str__(self):
+        return self._string_value
+
+    def __repr__(self):
+        return self._string_value
