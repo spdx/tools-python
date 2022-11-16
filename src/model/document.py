aad41ee1f333d0ce2a87a24a2ca1e3faa4458d7d
Nicolaus Weidner <nicolaus.weidner@tngtech.com>
2022-11-16 15:16:10 +0100

    [issue-290] Add new document class
    Signed-off-by: Nicolaus Weidner <nicolaus.weidner@tngtech.com>

---
+++ src/model/document.py
@@ -0,0 +1,76 @@
+# Copyright (c) 2022 spdx contributors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from datetime import datetime
+from typing import List, Optional
+
+from src.model.actor import Actor
+from src.model.annotation import Annotation
+from src.model.external_document_ref import ExternalDocumentRef
+from src.model.extracted_licensing_info import ExtractedLicensingInfo
+from src.model.file import File
+from src.model.package import Package
+from src.model.relationship import Relationship
+from src.model.snippet import Snippet
+from src.model.version import Version
+
+
+class CreationInfo:
+    creators: List[Actor]
+    created: datetime
+    comment: Optional[str]
+    license_list_version: Optional[Version]
+
+    def __init__(self, creators: List[Actor], created: datetime, comment: Optional[str] = None,
+                 license_list_version: Optional[Version] = None):
+        self.creators = creators
+        self.created = created
+        self.comment = comment
+        self.license_list_version = license_list_version
+
+
+class Document:
+    data_license = "CC0-1.0"
+    spdx_version: str
+    spdx_id: str
+    name: str
+    document_namespace: str
+    creation_info: CreationInfo
+    external_document_references: List[ExternalDocumentRef]
+    comment: Optional[str]
+
+    packages: List[Package]
+    files: List[File]
+    snippets: List[Snippet]
+    annotations: List[Annotation]
+    relationships: List[Relationship]
+    extracted_licensing_info: List[ExtractedLicensingInfo]
+
+    def __init__(self, spdx_version: str, spdx_id: str, name: str, document_namespace: str,
+                 creation_info: CreationInfo, external_document_references: List[ExternalDocumentRef] = None,
+                 comment: Optional[str] = None, packages: List[Package] = None, files: List[File] = None,
+                 snippets: List[Snippet] = None, annotations: List[Annotation] = None,
+                 relationships: List[Relationship] = None,
+                 extracted_licensing_info: List[ExtractedLicensingInfo] = None):
+        self.spdx_version = spdx_version
+        self.spdx_id = spdx_id
+        self.name = name
+        self.document_namespace = document_namespace
+        self.creation_info = creation_info
+        self.external_document_references = [] if external_document_references is None else external_document_references
+        self.comment = comment
+        self.packages = [] if packages is None else packages
+        self.files = [] if files is None else files
+        self.snippets = [] if snippets is None else snippets
+        self.annotations = [] if annotations is None else annotations
+        self.relationships = [] if relationships is None else relationships
+        self.extracted_licensing_info = [] if extracted_licensing_info is None else extracted_licensing_info
