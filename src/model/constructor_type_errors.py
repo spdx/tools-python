7866b0b8639bc38ba655467f02ec5e4cae843566
Nicolaus Weidner <nicolaus.weidner@tngtech.com>
2022-12-06 18:19:34 +0100

    [issue-348] Add new constructor type check utility so all TypeErrors are returned in one go
    Signed-off-by: Nicolaus Weidner <nicolaus.weidner@tngtech.com>

---
+++ src/model/constructor_type_errors.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class ConstructorTypeErrors(TypeError):
+    """
+    Helper class that holds a list of error messages. Intended to capture all TypeErrors encountered during a
+    constructor call, instead of raising only the first one.
+    """
+    messages: List[str]
+
+    def __init__(self, messages: List[str]):
+        self.messages = messages
