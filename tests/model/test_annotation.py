2ffc8bf50042e5ee78868f943c6d9c213eb3870a
Armin Tänzer <armin.taenzer@tngtech.com>
2022-12-06 14:38:31 +0100

    change annotator type to Actor and clean imports
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- tests/model/test_annotation.py
+++ tests/model/test_annotation.py
@@ -1,39 +1,46 @@
 from datetime import datetime
+from unittest import mock
 
 import pytest
 
 from src.model.annotation import Annotation, AnnotationType
 
 
-def test_correct_initialization():
-    annotation = Annotation("id", AnnotationType.OTHER, "annotator", datetime(2022, 1, 1), "comment")
+@mock.patch('src.model.actor.Actor', autospec=True)
+def test_correct_initialization(actor):
+    annotation = Annotation("id", AnnotationType.OTHER, actor, datetime(2022, 1, 1), "comment")
     assert annotation.spdx_id == "id"
     assert annotation.annotation_type == AnnotationType.OTHER
-    assert annotation.annotator == "annotator"
+    assert annotation.annotator == actor
     assert annotation.annotation_date == datetime(2022, 1, 1)
     assert annotation.annotation_comment == "comment"
 
 
-def test_wrong_type_in_spdx_id():
+@mock.patch('src.model.actor.Actor', autospec=True)
+def test_wrong_type_in_spdx_id(actor):
     with pytest.raises(TypeError):
-        Annotation(42, AnnotationType.OTHER, "annotator", datetime(2022, 1, 1), "comment")
+        Annotation(42, AnnotationType.OTHER, actor, datetime(2022, 1, 1), "comment")
 
 
-def test_wrong_type_in_annotation_type():
+@mock.patch('src.model.actor.Actor', autospec=True)
+def test_wrong_type_in_annotation_type(actor):
     with pytest.raises(TypeError):
-        Annotation("id", 42, "annotator", datetime(2022, 1, 1), "comment")
+        Annotation("id", 42, actor, datetime(2022, 1, 1), "comment")
 
 
-def test_wrong_type_in_annotator():
+@mock.patch('src.model.actor.Actor', autospec=True)
+def test_wrong_type_in_annotator(actor):
     with pytest.raises(TypeError):
         Annotation("id", AnnotationType.OTHER, 42, datetime(2022, 1, 1), "comment")
 
 
-def test_wrong_type_in_annotation_date():
+@mock.patch('src.model.actor.Actor', autospec=True)
+def test_wrong_type_in_annotation_date(actor):
     with pytest.raises(TypeError):
-        Annotation("id", AnnotationType.OTHER, "annotator", 42, "comment")
+        Annotation("id", AnnotationType.OTHER, actor, 42, "comment")
 
 
-def test_wrong_type_in_annotation_comment():
+@mock.patch('src.model.actor.Actor', autospec=True)
+def test_wrong_type_in_annotation_comment(actor):
     with pytest.raises(TypeError):
-        Annotation("id", AnnotationType.OTHER, "annotator", datetime(2022, 1, 1), 42)
+        Annotation("id", AnnotationType.OTHER, actor, datetime(2022, 1, 1), 42)
