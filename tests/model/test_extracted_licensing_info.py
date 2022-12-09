449bffa321c2c04158caa85455b69838bfbd8c02
Armin Tänzer <armin.taenzer@tngtech.com>
2022-12-09 17:35:42 +0100

    [refactor] move comment to the end of extractedLicensingInfo constructor
    this makes sense as it is the only unconditionally optional parameter
    
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- tests/model/test_extracted_licensing_info.py
+++ tests/model/test_extracted_licensing_info.py
@@ -4,12 +4,12 @@ from src.model.extracted_licensing_info import ExtractedLicensingInfo
 
 
 def test_correct_initialization():
-    extracted_licensing_info = ExtractedLicensingInfo("id", "text", "name", "comment", ["reference"])
+    extracted_licensing_info = ExtractedLicensingInfo("id", "text", "name", ["reference"], "comment")
     assert extracted_licensing_info.license_id == "id"
     assert extracted_licensing_info.extracted_text == "text"
     assert extracted_licensing_info.license_name == "name"
-    assert extracted_licensing_info.comment == "comment"
     assert extracted_licensing_info.cross_references == ["reference"]
+    assert extracted_licensing_info.comment == "comment"
 
 
 def test_wrong_type_in_license_id():
@@ -27,11 +27,11 @@ def test_wrong_type_in_license_name():
         ExtractedLicensingInfo(license_name=42)
 
 
-def test_wrong_type_in_comment():
-    with pytest.raises(TypeError):
-        ExtractedLicensingInfo(comment=42)
-
-
 def test_wrong_type_in_cross_references():
     with pytest.raises(TypeError):
         ExtractedLicensingInfo(cross_references=["ref", 42])
+
+
+def test_wrong_type_in_comment():
+    with pytest.raises(TypeError):
+        ExtractedLicensingInfo(comment=42)
