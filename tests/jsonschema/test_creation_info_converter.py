30c453bd6a28b7d74a36a781b06323f71d242105
Nicolaus Weidner <nicolaus.weidner@tngtech.com>
2022-12-15 13:36:19 +0100

    [issue-359] Add CreationInfo converter
    Signed-off-by: Nicolaus Weidner <nicolaus.weidner@tngtech.com>

---
+++ tests/jsonschema/test_creation_info_converter.py
@@ -0,0 +1,57 @@
+#  Copyright (c) 2022 spdx contributors
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from datetime import datetime
+
+import pytest
+
+from src.datetime_conversions import datetime_to_iso_string
+from src.jsonschema.creation_info_converter import CreationInfoConverter
+from src.jsonschema.creation_info_properties import CreationInfoProperty
+from src.model.actor import Actor, ActorType
+from src.model.document import CreationInfo
+from src.model.version import Version
+
+
+@pytest.fixture
+def converter() -> CreationInfoConverter:
+    return CreationInfoConverter()
+
+
+@pytest.mark.parametrize("creation_info_property,expected",
+                         [(CreationInfoProperty.CREATED, "created"), (CreationInfoProperty.CREATORS, "creators"),
+                          (CreationInfoProperty.LICENSE_LIST_VERSION, "licenseListVersion"),
+                          (CreationInfoProperty.COMMENT, "comment")])
+def test_json_property_names(converter: CreationInfoConverter, creation_info_property: CreationInfoProperty,
+                             expected: str):
+    assert converter.json_property_name(creation_info_property) == expected
+
+
+def test_successful_conversion(converter: CreationInfoConverter):
+    creators = [Actor(ActorType.PERSON, "personName"), Actor(ActorType.TOOL, "toolName")]
+    created = datetime(2022, 12, 1)
+    creation_info = CreationInfo("irrelevant", "irrelevant", "irrelevant", "irrelevant", creators=creators,
+                                 created=created, creator_comment="comment", license_list_version=Version(1, 2))
+
+    converted_dict = converter.convert(creation_info)
+
+    assert converted_dict[converter.json_property_name(CreationInfoProperty.CREATED)] == datetime_to_iso_string(created)
+    assert converted_dict[converter.json_property_name(CreationInfoProperty.CREATORS)] == ["Person: personName",
+                                                                                           "Tool: toolName"]
+    assert converted_dict[converter.json_property_name(CreationInfoProperty.LICENSE_LIST_VERSION)] == "1.2"
+    assert converted_dict[converter.json_property_name(CreationInfoProperty.COMMENT)] == "comment"
+
+
+def test_json_type(converter: CreationInfoConverter):
+    assert converter.get_json_type() == CreationInfoProperty
+
+
+def test_data_model_type(converter: CreationInfoConverter):
+    assert converter.get_data_model_type() == CreationInfo
