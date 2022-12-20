8f97552a6c7ec21a122a1ea1e376fac5dc676148
Nicolaus Weidner <nicolaus.weidner@tngtech.com>
2022-12-20 12:11:23 +0100

    [issue-359] add snippet converter
    Signed-off-by: Nicolaus Weidner <nicolaus.weidner@tngtech.com>

---
+++ src/jsonschema/snippet_converter.py
@@ -0,0 +1,88 @@
+#  Copyright (c) 2022 spdx contributors
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from typing import Type, Any, Tuple, Dict
+
+from src.jsonschema.annotation_converter import AnnotationConverter
+from src.jsonschema.converter import TypedConverter
+from src.jsonschema.json_property import JsonProperty
+from src.jsonschema.snippet_properties import SnippetProperty
+from src.model.document import Document
+from src.model.snippet import Snippet
+from src.writer.casing_tools import snake_case_to_camel_case
+
+
+class SnippetConverter(TypedConverter):
+    annotation_converter: AnnotationConverter
+
+    def __init__(self):
+        self.annotation_converter = AnnotationConverter()
+
+    def json_property_name(self, snippet_property: SnippetProperty) -> str:
+        if snippet_property == SnippetProperty.SPDX_ID:
+            return "SPDXID"
+        return snake_case_to_camel_case(snippet_property.name)
+
+    def _get_property_value(self, snippet: Snippet, snippet_property: SnippetProperty,
+                            document: Document = None) -> Any:
+        if snippet_property == SnippetProperty.SPDX_ID:
+            return snippet.spdx_id
+        elif snippet_property == SnippetProperty.ANNOTATIONS:
+            snippet_annotations = filter(lambda annotation: annotation.spdx_id == snippet.spdx_id, document.annotations)
+            return [self.annotation_converter.convert(annotation) for annotation in snippet_annotations]
+        elif snippet_property == SnippetProperty.ATTRIBUTION_TEXTS:
+            return snippet.attribution_texts
+        elif snippet_property == SnippetProperty.COMMENT:
+            return snippet.comment
+        elif snippet_property == SnippetProperty.COPYRIGHT_TEXT:
+            return snippet.copyright_text
+        elif snippet_property == SnippetProperty.LICENSE_COMMENTS:
+            return snippet.license_comment
+        elif snippet_property == SnippetProperty.LICENSE_CONCLUDED:
+            return str(snippet.concluded_license)
+        elif snippet_property == SnippetProperty.LICENSE_INFO_IN_SNIPPETS:
+            if isinstance(snippet.license_info_in_snippet, list):
+                return [str(license_expression) for license_expression in snippet.license_info_in_snippet]
+            return str(snippet.license_info_in_snippet)
+        elif snippet_property == SnippetProperty.NAME:
+            return snippet.name
+        elif snippet_property == SnippetProperty.RANGES:
+            ranges = [convert_byte_range_to_dict(snippet.byte_range, snippet.file_spdx_id)]
+            if snippet.line_range:
+                ranges.append(convert_line_range_to_dict(snippet.line_range, snippet.file_spdx_id))
+            return ranges
+        elif snippet_property == SnippetProperty.SNIPPET_FROM_FILE:
+            return snippet.file_spdx_id
+
+    def get_json_type(self) -> Type[JsonProperty]:
+        return SnippetProperty
+
+    def get_data_model_type(self) -> Type[Snippet]:
+        return Snippet
+
+    def requires_full_document(self) -> bool:
+        return True
+
+
+def convert_line_range_to_dict(line_range: Tuple[int, int], file_id: str) -> Dict:
+    return _convert_range_to_dict(line_range, file_id, "lineNumber")
+
+
+def convert_byte_range_to_dict(byte_range: Tuple[int, int], file_id: str) -> Dict:
+    return _convert_range_to_dict(byte_range, file_id, "offset")
+
+
+def _convert_range_to_dict(int_range: Tuple[int, int], file_id: str, pointer_property: str) -> Dict:
+    return {"startPointer": _pointer(file_id, int_range[0], pointer_property),
+            "endPointer": _pointer(file_id, int_range[1], pointer_property)}
+
+
+def _pointer(reference: str, target: int, pointer_property: str) -> Dict:
+    return {"reference": reference, pointer_property: target}
---
+++ tests/jsonschema/test_snippet_converter.py
@@ -0,0 +1,92 @@
+#  Copyright (c) 2022 spdx contributors
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from datetime import datetime
+from unittest import mock
+from unittest.mock import MagicMock
+
+import pytest
+
+from src.jsonschema.snippet_converter import SnippetConverter
+from src.jsonschema.snippet_properties import SnippetProperty
+from src.model.actor import Actor, ActorType
+from src.model.annotation import Annotation, AnnotationType
+from src.model.document import Document, CreationInfo
+from src.model.license_expression import LicenseExpression
+from src.model.snippet import Snippet
+
+
+@pytest.fixture
+@mock.patch('src.jsonschema.annotation_converter.AnnotationConverter', autospec=True)
+def converter(annotation_converter_mock: MagicMock) -> SnippetConverter:
+    converter = SnippetConverter()
+    converter.annotation_converter = annotation_converter_mock()
+    return converter
+
+
+@pytest.mark.parametrize("snippet_property,expected",
+                         [(SnippetProperty.SPDX_ID, "SPDXID"),
+                          (SnippetProperty.ANNOTATIONS, "annotations"),
+                          (SnippetProperty.ATTRIBUTION_TEXTS, "attributionTexts"),
+                          (SnippetProperty.COMMENT, "comment"),
+                          (SnippetProperty.COPYRIGHT_TEXT, "copyrightText"),
+                          (SnippetProperty.LICENSE_COMMENTS, "licenseComments"),
+                          (SnippetProperty.LICENSE_CONCLUDED, "licenseConcluded"),
+                          (SnippetProperty.LICENSE_INFO_IN_SNIPPETS, "licenseInfoInSnippets"),
+                          (SnippetProperty.NAME, "name"),
+                          (SnippetProperty.RANGES, "ranges"),
+                          (SnippetProperty.SNIPPET_FROM_FILE, "snippetFromFile")])
+def test_json_property_names(converter: SnippetConverter, snippet_property: SnippetProperty,
+                             expected: str):
+    assert converter.json_property_name(snippet_property) == expected
+
+
+def test_json_type(converter: SnippetConverter):
+    assert converter.get_json_type() == SnippetProperty
+
+
+def test_data_model_type(converter: SnippetConverter):
+    assert converter.get_data_model_type() == Snippet
+
+
+def test_successful_conversion(converter: SnippetConverter):
+    converter.annotation_converter.convert.return_value = "mock_converted_annotation"
+    file_spdx_id = "fileSpdxId"
+    snippet = Snippet("spdxId", file_spdx_id=file_spdx_id, byte_range=(1, 2), line_range=(3, 4),
+                      concluded_license=LicenseExpression("licenseExpression1"),
+                      license_info_in_snippet=[LicenseExpression("licenseExpression2"),
+                                               LicenseExpression("licenseExpression3")],
+                      license_comment="licenseComment", copyright_text="copyrightText", comment="comment", name="name",
+                      attribution_texts=["attributionText1", "attributionText2"])
+
+    annotation = Annotation(snippet.spdx_id, AnnotationType.OTHER, Actor(ActorType.PERSON, "annotatorName"),
+                            datetime(2022, 12, 5), "other comment")
+    creation_info = CreationInfo("spdxVersion", "documentId", "documentName", "documentNamespace", [],
+                                 datetime(2022, 12, 4))
+    document = Document(creation_info, snippets=[snippet], annotations=[annotation])
+    converted_dict = converter.convert(snippet, document)
+
+    assert converted_dict[converter.json_property_name(SnippetProperty.SPDX_ID)] == "spdxId"
+    assert converted_dict[converter.json_property_name(SnippetProperty.ANNOTATIONS)] == ["mock_converted_annotation"]
+    assert converted_dict[converter.json_property_name(SnippetProperty.ATTRIBUTION_TEXTS)] == ["attributionText1",
+                                                                                               "attributionText2"]
+    assert converted_dict[converter.json_property_name(SnippetProperty.COMMENT)] == "comment"
+    assert converted_dict[converter.json_property_name(SnippetProperty.COPYRIGHT_TEXT)] == "copyrightText"
+    assert converted_dict[converter.json_property_name(SnippetProperty.LICENSE_COMMENTS)] == "licenseComment"
+    assert converted_dict[converter.json_property_name(SnippetProperty.LICENSE_CONCLUDED)] == "licenseExpression1"
+    assert converted_dict[converter.json_property_name(SnippetProperty.LICENSE_INFO_IN_SNIPPETS)] == [
+        "licenseExpression2", "licenseExpression3"]
+    assert converted_dict[converter.json_property_name(SnippetProperty.NAME)] == "name"
+    assert converted_dict[converter.json_property_name(SnippetProperty.RANGES)] == [
+        {"startPointer": {"reference": file_spdx_id, "offset": 1},
+         "endPointer": {"reference": file_spdx_id, "offset": 2}},
+        {"startPointer": {"reference": file_spdx_id, "lineNumber": 3},
+         "endPointer": {"reference": file_spdx_id, "lineNumber": 4}}]
+    assert converted_dict[converter.json_property_name(SnippetProperty.SNIPPET_FROM_FILE)] == file_spdx_id
