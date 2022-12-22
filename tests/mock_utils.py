7aa1b74f613cfa0f83fceebf2ae8db969c4dfbc3
Nicolaus Weidner <nicolaus.weidner@tngtech.com>
2022-12-22 23:50:55 +0100

    [issue-359] Make mock utilities compatible with Python 3.7
    Signed-off-by: Nicolaus Weidner <nicolaus.weidner@tngtech.com>

--- tests/mock_utils.py
+++ tests/mock_utils.py
@@ -16,7 +16,7 @@ def assert_mock_method_called_with_arguments(mock_object: NonCallableMagicMock, 
     for running_index in range(len(args)):
         call = mock_object.method_calls[running_index]
         assert call[0] == method_name
-        assert call.args[0] == args[running_index]
+        assert call[1][0] == args[running_index]
 
 
 def assert_no_mock_methods_called(mock_object: NonCallableMagicMock):
