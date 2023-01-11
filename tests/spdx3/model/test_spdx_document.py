d9ec61a2e13194a766eacbea9b61a6bbe49d1066
Meret Behrens <meret.behrens@tngtech.com>
2023-01-11 15:27:19 +0100

    [spdx3.0] add tests for data model and fix initialization for inherited classes
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

---
+++ tests/spdx3/model/test_spdx_document.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2023 spdx contributors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from unittest import mock
+
+import pytest
+
+from spdx3.model.spdx_document import SpdxDocument
+
+
+@mock.patch("spdx3.model.creation_information.CreationInformation", autospec=True)
+def test_correct_initialization(creation_information):
+    spdx_document = SpdxDocument("SPDXRef-DOCUMENT", creation_information, "Test document")
+
+    assert spdx_document.spdx_id == "SPDXRef-DOCUMENT"
+    assert spdx_document.creation_info == creation_information
+    assert spdx_document.name == "Test document"
+
+
+def test_invalid_initialization():
+    with pytest.raises(TypeError) as err:
+        SpdxDocument(1, {"info": 5}, "document name")
+
+    assert err.value.args[0] == ['SetterError SpdxDocument: type of argument "spdx_id" must be str; got int '
+                                 'instead: 1',
+                                 'SetterError SpdxDocument: type of argument "creation_info" must be '
+                                 'spdx3.model.creation_information.CreationInformation; got dict instead: '
+                                 "{'info': 5}"]
+
+@mock.patch("spdx3.model.creation_information.CreationInformation", autospec=True)
+def test_incomplete_initialization(creation_information):
+    with pytest.raises(TypeError) as err:
+        SpdxDocument("SPDXRef-Docuement", creation_information)
+
+    assert err.value.args[0] == "SpdxDocument.__init__() missing 1 required positional argument: 'name'"
