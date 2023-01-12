9d171782aa80e78e9c3e8e6d1392521042ffb192
Meret Behrens <meret.behrens@tngtech.com>
2023-01-12 12:17:45 +0100

    [spdx3.0] add basic conversion from document 2.x to 3.0
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

---
+++ src/conversion/convert_creation_information.py
@@ -0,0 +1,49 @@
+#  Copyright (c) 2023 spdx contributors
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#    http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from datetime import datetime
+
+from semantic_version import Version
+
+from conversion.message import print_missing_conversion
+from spdx3.model.spdx_document import SpdxDocument
+
+from spdx3.model.creation_information import CreationInformation
+
+from spdx.model.document import CreationInfo
+
+
+def convert_creation_information(creation_info: CreationInfo) -> SpdxDocument:
+    # creation_info.spdx_id -> spdx_document.spdx_id
+    spdx_id = creation_info.spdx_id
+
+    # creation_info.name -> spdx_document.name
+    name = creation_info.name
+
+    # creation_info.document_namespace -> ?
+    print("\n")
+    print_missing_conversion("creation_info.document_namespace", 0)
+    # creation_info.creators -> creation_information.creators (not implemented yet)
+    print_missing_conversion("creation_info.creators", 1, "of creators")
+    created: datetime = creation_info.created
+    # creation_info.creator_comment -> ?
+    print_missing_conversion("creation_info.creator_comment", 0)
+    data_license = creation_info.data_license
+    # creation_info.external_document_refs -> spdx_document.imports
+    imports = creation_info.external_document_refs
+    print_missing_conversion("creation_info.external_document_refs", 0, "ExternalDocumentRef -> ExternalMap")
+    # creation_info.license_list_version -> ?
+    print_missing_conversion("creation_info.license_list_version",0)
+    # creation_info.document_comment -> spdx_document.comment
+    document_comment = creation_info.document_comment
+    creation_information = CreationInformation(Version("3.0.0"), created, None, [], data_license)
+    spdx_document = SpdxDocument(spdx_id=spdx_id, creation_info=creation_information, name=name, comment=document_comment)
+
+    return spdx_document
