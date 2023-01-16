ccc81d14e9a9b3d81f54e12ec721d3d7af28dd8a
Meret Behrens <meret.behrens@tngtech.com>
2023-01-16 17:11:46 +0100

    [spdx3.0] add IntegrityMethod
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx3/model/external_map.py
+++ src/spdx3/model/external_map.py
@@ -8,17 +8,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Optional
+from dataclasses import field
+from typing import Optional, List
 from common.typing.type_checks import check_types_and_set_values
 
 from common.typing.dataclass_with_properties import dataclass_with_properties
+from spdx3.model.integrity_method import IntegrityMethod
 
 
 @dataclass_with_properties
 class ExternalMap:
     external_id: str  # anyURI
-    verified_using: None  # placeholder for IntegrityMethod
+    verified_using: Optional[List[IntegrityMethod]] = field(default_factory=list)
     location_hint: Optional[str] = None  # anyURI
 
-    def __init__(self, external_id: str, verified_using: None, location_hint: Optional[str] = None):
+    def __init__(self, external_id: str, verified_using: Optional[List[IntegrityMethod]]= None, location_hint: Optional[str] = None):
+        verified_using = [] if verified_using is None else verified_using
         check_types_and_set_values(self, locals())
