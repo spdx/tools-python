715ca6c34348d0cc23532ac17616110088a2a9e6
Meret Behrens <meret.behrens@tngtech.com>
2023-01-17 14:15:00 +0100

    [refactor] move each class in a separate file
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx3/model/integrity_method.py
+++ src/spdx3/model/integrity_method.py
@@ -8,7 +8,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from enum import Enum, auto
 from typing import Optional
 
 from common.typing.dataclass_with_properties import dataclass_with_properties
@@ -22,39 +21,4 @@ class IntegrityMethod:
     comment: Optional[str] = None
 
     def __init__(self, comment: Optional[str] = None):
-        check_types_and_set_values(self, locals())
-
-
-class HashAlgorithm(Enum):
-    BLAKE2B256 = auto()
-    BLAKE2B384 = auto()
-    BLAKE2B512 = auto()
-    BLAKE3 = auto()
-    MD2 = auto()
-    MD4 = auto()
-    MD5 = auto()
-    MD6 = auto()
-    OTHER = auto()
-    SHA1 = auto()
-    SHA224 = auto()
-    SHA256 = auto()
-    SHA3_224 = auto()
-    SHA3_256 = auto()
-    SHA3_384 = auto()
-    SHA3_512 = auto()
-    SHA384 = auto()
-    SHA512 = auto()
-    SPDXPVCSHA1 = auto()
-    SPDXPVCSHA256 = auto()
-
-
-@dataclass_with_properties
-class Hash(IntegrityMethod):
-    algorithm: HashAlgorithm = None
-    hash_value: str = None
-    """We overwrite the constructor of the inherited class so that all fields (including the fields from the parent
-    class) are set. Pycharm (and probably also other IDEs) warns about a missing call to the constructor of the super 
-    class but as we have taken care of all fields this warning can be ignored."""
-
-    def __init__(self, algorithm: HashAlgorithm, hash_value: str, comment: Optional[str] = None):
         check_types_and_set_values(self, locals())
---
+++ tests/spdx3/model/test_artifact.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2023 spdx contributors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#    http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from unittest import mock
+
+import pytest
+
+from spdx3.model.artifact import Artifact
+
+
+@mock.patch("spdx3.model.creation_information.CreationInformation", autospec=True)
+def test_correct_initialization_artifact(creation_info):
+    artifact = Artifact("SPDXRef-Artifact", creation_info, originated_by=None)
+
+    assert artifact.spdx_id == "SPDXRef-Artifact"
+    assert artifact.creation_info == creation_info
+    assert artifact.originated_by is None
+
+
+@mock.patch("spdx3.model.creation_information.CreationInformation", autospec=True)
+def test_invalid_initialization_artifact(creation_info):
+    with pytest.raises(TypeError) as err:
+        Artifact(65, creation_info, originated_by=54)
+
+    assert err.value.args[0] == ['SetterError Artifact: type of argument "spdx_id" must be str; got int '
+                                 'instead: 65',
+                                 'SetterError Artifact: type of argument "originated_by" must be NoneType; got '
+                                 'int instead: 54']
