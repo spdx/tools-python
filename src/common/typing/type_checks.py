7e7c052c3d3acffb1c694400b5f50466b3f7a3b1
Meret Behrens <meret.behrens@tngtech.com>
2023-01-17 08:47:45 +0100

    [spdx3.0] refactor: use method instead of private variable to iterate through the fields of a dataclass and add license header
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/common/typing/type_checks.py
+++ src/common/typing/type_checks.py
@@ -1,3 +1,14 @@
+# Copyright (c) 2022 spdx contributors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from dataclasses import fields
 from typing import Any, Dict
 
 from common.typing.constructor_type_errors import ConstructorTypeErrors
@@ -12,11 +23,10 @@ def check_types_and_set_values(instance_under_construction: Any, local_variables
     return all type violations in one go.
     As an aside, defining constructors "manually" using this utility method helps avoid a nasty PyCharm bug:
     https://youtrack.jetbrains.com/issue/PY-34569
-    With the additional parameter origin_class we ensure that the attributes from the class that calls this method
-    are set. If we use inheritance the instance_under_construction object might be a child object.
     """
     errors = []
-    for key in instance_under_construction.__dataclass_fields__.keys():
+    for field in fields(instance_under_construction):
+        key = field.name
         value = local_variables.get(key)
         try:
             setattr(instance_under_construction, key, value)
