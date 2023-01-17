2bf65d3dc5e45d6623b4d7074d75606b7d19249a
Meret Behrens <meret.behrens@tngtech.com>
2023-01-17 09:09:54 +0100

    [spdx3.0] add checksum bump
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx3/bump_from_spdx2/file.py
+++ src/spdx3/bump_from_spdx2/file.py
@@ -8,6 +8,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from spdx3.bump_from_spdx2.checksum import bump_checksum
 from spdx3.bump_from_spdx2.message import print_missing_conversion
 from spdx3.model.software.file import File
 
@@ -19,9 +20,9 @@ from spdx.model.file import File as Spdx2_File
 def bump_file(spdx2_file: Spdx2_File, creation_information: CreationInformation) -> File:
     name = spdx2_file.name
     spdx_id = spdx2_file.spdx_id
+    integrity_methods = [bump_checksum(checksum) for checksum in spdx2_file.checksums]
     # file.checksums -> file.verifiedUsing
     print("\n")
-    print_missing_conversion("file.checksums", 1, "for IntegrityMethod")
     # file.file_types -> file.content_type (MediaType with Cardinality 1)
     print_missing_conversion("file.file_type", 0, "different cardinalities")
     print_missing_conversion(
@@ -32,5 +33,6 @@ def bump_file(spdx2_file: Spdx2_File, creation_information: CreationInformation)
     print_missing_conversion("file.notice, file.contributors, file.attribution_texts", 0,
                              "missing definition for license profile")
 
-    file = File(spdx_id, creation_info=creation_information, name=name, comment=comment)
+    file = File(spdx_id, creation_info=creation_information, name=name, comment=comment,
+                verified_using=integrity_methods)
     return file
