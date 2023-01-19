7e7e35eea8b3992a0d41780ec595b52c485f9d57
Meret Behrens <meret.behrens@tngtech.com>
2023-01-19 16:34:58 +0100

    [spdx3.0, writer] fix output
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx3/writer/console/relationship_writer.py
+++ src/spdx3/writer/console/relationship_writer.py
@@ -12,10 +12,12 @@ from typing import TextIO
 
 from spdx3.model.relationship import Relationship
 from spdx3.writer.console.console import write_value
+from spdx3.writer.console.element_writer import write_element_properties
 
 
 def write_relationship(relationship: Relationship, text_output: TextIO):
     text_output.write("## Relationship\n")
+    write_element_properties(relationship, text_output)
     write_value("from_element", relationship.from_element, text_output)
     write_value("to", ", ".join(relationship.to), text_output)
     write_value("relationship_type", relationship.relationship_type.name, text_output)
