da076864b3f04c5b7c7e81ece1ebfbf9d03f0ce1
Meret Behrens <meret.behrens@tngtech.com>
2023-01-19 17:05:29 +0100

    [spdx-3.0, writer] add indent for nested elements
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx3/writer/console/hash_writer.py
+++ src/spdx3/writer/console/hash_writer.py
@@ -15,9 +15,9 @@ from spdx3.writer.console.console import write_value
 from spdx3.writer.console.integrity_method_writer import write_integrity_method
 
 
-def write_hash(hash_object: Hash, text_output: TextIO, heading: bool):
+def write_hash(hash_object: Hash, text_output: TextIO, heading: bool, indent: bool = True):
     if heading:
         text_output.write("## Hash\n")
-    write_value("algorithm", hash_object.algorithm.name, text_output)
-    write_value("hash_value", hash_object.hash_value, text_output)
-    write_integrity_method(hash_object, text_output)
+    write_value("algorithm", hash_object.algorithm.name, text_output, indent)
+    write_value("hash_value", hash_object.hash_value, text_output, indent)
+    write_integrity_method(hash_object, text_output, indent)
