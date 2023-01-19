ce8a0f7b7a69f252d8d6a920bb0897825ba7e36e
Meret Behrens <meret.behrens@tngtech.com>
2023-01-19 11:27:37 +0100

    [fix] update type hint for verified_using
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx3/model/software/package.py
+++ src/spdx3/model/software/package.py
@@ -16,22 +16,24 @@ from common.typing.type_checks import check_types_and_set_values
 
 from common.typing.dataclass_with_properties import dataclass_with_properties
 from spdx3.model.artifact import Artifact
+from spdx3.model.integrity_method import IntegrityMethod
 from spdx3.model.software.software_purpose import SoftwarePurpose
 
 
 @dataclass_with_properties
 class Package(Artifact):
-    content_identifier: Optional[str]  = None # anyURI
+    content_identifier: Optional[str] = None  # anyURI
     package_purpose: Optional[List[SoftwarePurpose]] = None
-    download_location: Optional[str] = None # anyURI
-    package_uri: Optional[str] = None # anyURI
-    homepage: Optional[str] = None # anyURI
+    download_location: Optional[str] = None  # anyURI
+    package_uri: Optional[str] = None  # anyURI
+    homepage: Optional[str] = None  # anyURI
 
     def __init__(self, spdx_id: str, creation_info: CreationInformation, name: Optional[str] = None,
                  summary: Optional[str] = None, description: Optional[str] = None, comment: Optional[str] = None,
-                 verified_using: None = None, external_references: None = None, external_identifier: None = None,
-                 extension: None = None, originated_by: None = None, content_identifier: Optional[str] = None,
-                 package_purpose: Optional[List[SoftwarePurpose]] = None, download_location: Optional[str] = None,
-                 package_uri: Optional[str] = None, homepage: Optional[str] = None):
+                 verified_using: Optional[List[IntegrityMethod]] = None, external_references: None = None,
+                 external_identifier: None = None, extension: None = None, originated_by: None = None,
+                 content_identifier: Optional[str] = None, package_purpose: Optional[List[SoftwarePurpose]] = None,
+                 download_location: Optional[str] = None, package_uri: Optional[str] = None,
+                 homepage: Optional[str] = None):
         package_purpose = [] if package_purpose is None else package_purpose
         check_types_and_set_values(self, locals())
