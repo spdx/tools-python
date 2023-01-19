be151cf9ebce4f7ed4607a11d4291dcda0a84551
Meret Behrens <meret.behrens@tngtech.com>
2023-01-19 16:34:10 +0100

    [spdx3.0, fix] add type hint for external_references, set empty list per default
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx3/model/relationship.py
+++ src/spdx3/model/relationship.py
@@ -11,6 +11,8 @@
 from enum import Enum, auto
 from typing import List, Optional
 
+from spdx3.model.external_reference import ExternalReference
+
 from common.typing.type_checks import check_types_and_set_values
 
 from spdx3.model.creation_information import CreationInformation
@@ -81,8 +83,9 @@ class Relationship(Element):
     def __init__(self, spdx_id: str, creation_info: CreationInformation, from_element: str, to: List[str],
                  relationship_type: RelationshipType, name: Optional[str] = None, summary: Optional[str] = None,
                  description: Optional[str] = None, comment: Optional[str] = None,
-                 verified_using: Optional[List[IntegrityMethod]] = None, external_references: None = None,
-                 external_identifier: None = None, extension: None = None,
-                 completeness: Optional[RelationshipCompleteness] = None):
+                 verified_using: Optional[List[IntegrityMethod]] = None,
+                 external_references: Optional[List[ExternalReference]] = None, external_identifier: None = None,
+                 extension: None = None, completeness: Optional[RelationshipCompleteness] = None):
         verified_using = [] if verified_using is None else verified_using
+        external_references = [] if external_references is None else external_references
         check_types_and_set_values(self, locals())
