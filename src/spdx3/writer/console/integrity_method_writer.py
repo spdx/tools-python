4243e6f8423f73bd02830dcf3a5d76e8adaa16f1
Meret Behrens <meret.behrens@tngtech.com>
2023-01-19 17:05:29 +0100

    [spdx-3.0, writer] add indent for nested elements
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx3/writer/console/integrity_method_writer.py
+++ src/spdx3/writer/console/integrity_method_writer.py
@@ -14,5 +14,5 @@ from spdx3.model.integrity_method import IntegrityMethod
 from spdx3.writer.console.console import write_value
 
 
-def write_integrity_method(integrity_method: IntegrityMethod, text_output: TextIO):
-    write_value("comment", integrity_method.comment, text_output)
+def write_integrity_method(integrity_method: IntegrityMethod, text_output: TextIO, indent: bool = True):
+    write_value("comment", integrity_method.comment, text_output, indent)
