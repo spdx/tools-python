f66b3f05d5ad6dbc36045f11ddaff6d8da2579e6
Meret Behrens <meret.behrens@tngtech.com>
2023-01-19 16:34:58 +0100

    [spdx3.0, writer] fix output
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx3/writer/console/annotation_writer.py
+++ src/spdx3/writer/console/annotation_writer.py
@@ -20,6 +20,6 @@ def write_annotation(annotation: Annotation, text_output: TextIO):
     text_output.write("## Annotation\n")
     write_element_properties(annotation, text_output)
     write_value("annotation_type", annotation.annotation_type.name, text_output)
-    text_output.write(f"# subjects: {', '.join(annotation.subject)}")
+    text_output.write(f"subjects: {', '.join(annotation.subject)}\n")
     write_value("content_type", annotation.content_type, text_output)
     write_value("statement", annotation.statement, text_output)
