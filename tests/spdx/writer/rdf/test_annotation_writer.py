6b0129a60e5da02f32c89f12385ca7ace0ba456c
Meret Behrens <meret.behrens@tngtech.com>
2023-01-24 12:26:12 +0100

    [issue-407] add writer for annotations
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

---
+++ tests/spdx/writer/rdf/test_annotation_writer.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2023 spdx contributors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from datetime import datetime
+
+from rdflib import Graph, Literal, RDFS
+
+from spdx.datetime_conversions import datetime_to_iso_string
+from spdx.model.annotation import AnnotationType
+from spdx.writer.rdf.annotation_writer import add_annotation_info_to_graph
+from spdx.writer.rdf.writer_utils import spdx_namespace
+from tests.spdx.fixtures import annotation_fixture
+
+
+def test_add_annotation_info_to_graph():
+    graph = Graph()
+    annotation = annotation_fixture()
+
+    add_annotation_info_to_graph(annotation, graph)
+
+    assert (None, None, spdx_namespace().Annotation) in graph
+    assert (None, spdx_namespace().annotationType, Literal(AnnotationType.REVIEW.name)) in graph
+    assert (None, spdx_namespace().annotationDate, Literal(datetime_to_iso_string(datetime(2022, 12, 1)))) in graph
+    assert (None, spdx_namespace().annotator, Literal("Person: annotatorName"))
+    assert (None, RDFS.comment, Literal("annotationComment")) in graph
