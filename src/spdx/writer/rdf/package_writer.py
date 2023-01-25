d2534c7a3f391c6042fd51eafbed0e79fa998ef7
Meret Behrens <meret.behrens@tngtech.com>
2023-01-25 14:17:48 +0100

    [issue-407] add package writer
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

---
+++ src/spdx/writer/rdf/package_writer.py
@@ -0,0 +1,94 @@
+# Copyright (c) 2023 spdx contributors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import List
+
+from rdflib import Graph, URIRef, RDF, Literal, XSD, BNode, DOAP, RDFS, Namespace
+
+from spdx.writer.casing_tools import snake_case_to_camel_case
+from spdx.writer.rdf.checksum_writer import add_checksum_information_to_graph
+
+from spdx.model.package import Package, PackageVerificationCode, ExternalPackageRef
+from spdx.writer.rdf.writer_utils import spdx_namespace, add_literal_value, add_literal_or_no_assertion_or_none, \
+    add_datetime_to_graph
+
+
+def add_package_information_to_graph(package: Package, graph: Graph, doc_namespace: str):
+    package_resource = URIRef(f"{doc_namespace}#{package.spdx_id}")
+    graph.add((package_resource, RDF.type, spdx_namespace.Package))
+
+    graph.add((package_resource, spdx_namespace.name, Literal(package.name)))
+    add_literal_value(graph, package_resource, spdx_namespace.versionInfo, package.version)
+    add_literal_value(graph, package_resource, spdx_namespace.packageFileName, package.file_name)
+    add_literal_value(graph, package_resource, spdx_namespace.supplier, package.supplier)
+    add_literal_value(graph, package_resource, spdx_namespace.originator, package.originator)
+    add_literal_or_no_assertion_or_none(graph, package_resource, spdx_namespace.downloadLocation,
+                                        package.download_location)
+    graph.add((package_resource, spdx_namespace.filesAnalyzed, Literal(package.files_analyzed, datatype=XSD.boolean)))
+    add_package_verification_code_to_graph(package.verification_code, graph, package_resource)
+    for checksum in package.checksums:
+        add_checksum_information_to_graph(checksum, graph, package_resource)
+
+    add_literal_value(graph, package_resource, DOAP.homepage, package.homepage)
+    add_literal_value(graph, package_resource, spdx_namespace.sourceInfo, package.source_info)
+    add_literal_or_no_assertion_or_none(graph, package_resource, spdx_namespace.licenseConcluded,
+                                        package.license_concluded)
+    add_literal_or_no_assertion_or_none(graph, package_resource, spdx_namespace.licenseInfoFromFiles,
+                                        package.license_info_from_files)
+    add_literal_or_no_assertion_or_none(graph, package_resource, spdx_namespace.licenseDeclared,
+                                        package.license_declared)
+    add_literal_value(graph, package_resource, spdx_namespace.licenseComments, package.license_comment)
+    add_literal_value(graph, package_resource, spdx_namespace.copyrightText, package.copyright_text)
+    add_literal_value(graph, package_resource, spdx_namespace.summary, package.summary)
+    add_literal_value(graph, package_resource, spdx_namespace.description, package.description)
+    add_literal_value(graph, package_resource, RDFS.comment, package.comment)
+    for external_reference in package.external_references:
+        add_external_package_ref_to_graph(graph, external_reference, package_resource)
+    for attribution_text in package.attribution_texts:
+        add_literal_value(graph, package_resource, spdx_namespace.attributionText, attribution_text)
+    if package.primary_package_purpose:
+        graph.add((package_resource, spdx_namespace.primaryPackagePurpose,
+                   spdx_namespace[f"packagePurpose_{snake_case_to_camel_case(package.primary_package_purpose.name)}"]))
+
+    add_datetime_to_graph(graph, package_resource, spdx_namespace.releaseDate, package.release_date)
+    add_datetime_to_graph(graph, package_resource, spdx_namespace.builtDate, package.built_date)
+    add_datetime_to_graph(graph, package_resource, spdx_namespace.validUntilDate, package.valid_until_date)
+
+
+def add_package_verification_code_to_graph(package_verification_code: PackageVerificationCode, graph: Graph,
+                                           package_resource: URIRef):
+    if not package_verification_code:
+        return
+    package_verification_code_node = BNode()
+    graph.add((package_verification_code_node, RDF.type, spdx_namespace.PackageVerificationCode))
+    graph.add((package_verification_code_node, spdx_namespace.packageVerificationCodeValue,
+               Literal(package_verification_code.value)))
+    for excluded_file in package_verification_code.excluded_files:
+        graph.add((package_verification_code_node, spdx_namespace.packageVerificationCodeExcludedFile,
+                   Literal(excluded_file)))
+
+    graph.add((package_resource, spdx_namespace.packageVerificationCode, package_verification_code_node))
+
+
+def add_external_package_ref_to_graph(graph: Graph, external_package_ref: ExternalPackageRef,
+                                      package_resource: URIRef):
+    external_package_ref_node = BNode()
+    graph.add((external_package_ref_node, RDF.type, spdx_namespace.ExternalRef))
+    graph.add((external_package_ref_node, spdx_namespace.referenceCategory,
+               spdx_namespace[f"referenceCategory_{snake_case_to_camel_case(external_package_ref.category.name)}"]))
+    # the referenceType should either be f"http://spdx.org/rdf/references/{location}" for listed locations
+    # or f"{doc_namespace}#type" for unlisted locations, as a first attempt we simply write a Literal
+    # TODO: open issue
+    graph.add((external_package_ref_node, spdx_namespace.referenceType, Literal(external_package_ref.reference_type)))
+    graph.add((external_package_ref_node, spdx_namespace.referenceLocator, Literal(external_package_ref.locator)))
+    if external_package_ref.comment:
+        graph.add((external_package_ref_node, RDFS.comment, Literal(external_package_ref.comment)))
+
+    graph.add((package_resource, spdx_namespace.externalRef, external_package_ref_node))
---
+++ tests/spdx/writer/rdf/test_package_writer.py
@@ -0,0 +1,76 @@
+# Copyright (c) 2023 spdx contributors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from datetime import datetime
+
+from rdflib import Graph, URIRef, RDF, Literal, XSD, RDFS, DOAP
+
+from spdx.datetime_conversions import datetime_to_iso_string
+from spdx.writer.rdf.package_writer import add_package_information_to_graph, add_external_package_ref_to_graph, \
+    add_package_verification_code_to_graph
+from spdx.writer.rdf.writer_utils import spdx_namespace
+from tests.spdx.fixtures import package_fixture, external_package_ref_fixture, package_verification_code_fixture
+
+
+def test_add_package_information_to_graph():
+    graph = Graph()
+    package = package_fixture()
+
+    add_package_information_to_graph(package, graph, "anyURI")
+
+    assert (URIRef("anyURI#SPDXRef-Package"), RDF.type, spdx_namespace.Package) in graph
+    assert (None, spdx_namespace.name, Literal("packageName")) in graph
+    assert (None, spdx_namespace.versionInfo, Literal("12.2")) in graph
+    assert (None, spdx_namespace.packageFileName, Literal("./packageFileName")) in graph
+    assert (None, spdx_namespace.supplier, Literal("Person: supplierName (some@mail.com)")) in graph
+    assert (None, spdx_namespace.originator, Literal("Person: originatorName (some@mail.com)")) in graph
+    assert (None, spdx_namespace.downloadLocation, Literal("https://download.com")) in graph
+    assert (None, spdx_namespace.filesAnalyzed, Literal("true", datatype=XSD.boolean)) in graph
+    assert (URIRef("anyURI#SPDXRef-Package"), spdx_namespace.packageVerificationCode, None) in graph
+    assert (URIRef("anyURI#SPDXRef-Package"), spdx_namespace.checksum, None) in graph
+    assert (None, DOAP.homepage, Literal("https://homepage.com")) in graph
+    assert (None, spdx_namespace.sourceInfo, Literal("sourceInfo")) in graph
+    assert (None, spdx_namespace.licenseConcluded, Literal("packageLicenseConcluded")) in graph
+    assert (None, spdx_namespace.licenseInfoFromFiles, Literal("licenseInfoFromFile")) in graph
+    assert (None, spdx_namespace.licenseDeclared, Literal("packageLicenseDeclared")) in graph
+    assert (None, spdx_namespace.licenseComments, Literal("packageLicenseComment")) in graph
+    assert (None, spdx_namespace.copyrightText, Literal("packageCopyrightText")) in graph
+    assert (None, spdx_namespace.summary, Literal("packageSummary")) in graph
+    assert (None, spdx_namespace.description, Literal("packageDescription")) in graph
+    assert (None, RDFS.comment, Literal("packageComment")) in graph
+    assert (URIRef("anyURI#SPDXRef-Package"), spdx_namespace.externalRef, None) in graph
+    assert (None, spdx_namespace.attributionText, Literal("packageAttributionText"))
+    assert (None, spdx_namespace.primaryPackagePurpose, spdx_namespace.primaryPackagePurpose_source)
+    assert (None, spdx_namespace.releaseDate, Literal(datetime_to_iso_string(datetime(2022, 12, 1)))) in graph
+    assert (None, spdx_namespace.builtDate, Literal(datetime_to_iso_string(datetime(2022, 12, 2)))) in graph
+    assert (None, spdx_namespace.validUntilDate, Literal(datetime_to_iso_string(datetime(2022, 12, 3)))) in graph
+
+def test_add_package_verification_code_to_graph():
+    graph = Graph()
+    verification_code = package_verification_code_fixture()
+
+    add_package_verification_code_to_graph(verification_code, graph, URIRef("anyURI"))
+
+    assert (None, None, spdx_namespace.PackageVerificationCode) in graph
+    assert (None, spdx_namespace.packageVerificationCodeValue, Literal("85ed0817af83a24ad8da68c2b5094de69833983c")) in graph
+    assert (None, spdx_namespace.packageVerificationCodeExcludedFile, Literal("./exclude.py")) in graph
+
+
+def test_external_package_ref_to_graph():
+    graph = Graph()
+    external_reference = external_package_ref_fixture()
+
+    add_external_package_ref_to_graph(graph, external_reference, URIRef("anyURI"))
+
+    assert (None, None, spdx_namespace.ExternalRef) in graph
+    assert (None, spdx_namespace.referenceCategory, spdx_namespace.referenceCategory_packageManager) in graph
+    assert (None, spdx_namespace.referenceType, Literal("maven-central") ) in graph
+    assert (None, spdx_namespace.referenceLocator, Literal("org.apache.tomcat:tomcat:9.0.0.M4")) in graph
+    assert (None, RDFS.comment, Literal("externalPackageRefComment")) in graph
