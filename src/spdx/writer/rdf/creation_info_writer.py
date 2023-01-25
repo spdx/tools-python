2dc5fa6ce46b7cd5a0982f6027f33999a3f5b6fb
Meret Behrens <meret.behrens@tngtech.com>
2023-01-25 08:56:05 +0100

    [issue-407, refactor] rename function
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx/writer/rdf/creation_info_writer.py
+++ src/spdx/writer/rdf/creation_info_writer.py
@@ -12,7 +12,7 @@ from rdflib import Graph, BNode, RDF, Literal, RDFS, URIRef
 
 from spdx.datetime_conversions import datetime_to_iso_string
 from spdx.model.document import CreationInfo
-from spdx.writer.rdf.writer_utils import spdx_namespace, add_literal_value_if_exists
+from spdx.writer.rdf.writer_utils import spdx_namespace, add_literal_value
 
 
 def add_creation_info_to_graph(creation_info: CreationInfo, graph: Graph):
@@ -21,7 +21,7 @@ def add_creation_info_to_graph(creation_info: CreationInfo, graph: Graph):
     graph.add((doc_node, spdx_namespace.specVersion, Literal(creation_info.spdx_version)))
     graph.add((doc_node, spdx_namespace.dataLicense, Literal(creation_info.data_license)))
     graph.add((doc_node, spdx_namespace.name, Literal(creation_info.name)))
-    add_literal_value_if_exists(graph, doc_node, RDFS.comment, creation_info.document_comment)
+    add_literal_value(graph, doc_node, RDFS.comment, creation_info.document_comment)
 
     creation_info_node = BNode()
     graph.add((creation_info_node, RDF.type, spdx_namespace.CreationInfo))
@@ -31,8 +31,8 @@ def add_creation_info_to_graph(creation_info: CreationInfo, graph: Graph):
     for creator in creation_info.creators:
         graph.add((creation_info_node, spdx_namespace.creator, Literal(creator.to_serialized_string())))
 
-    add_literal_value_if_exists(graph, creation_info_node, spdx_namespace.licenseListVersion, creation_info.license_list_version)
-    add_literal_value_if_exists(graph, creation_info_node, RDFS.comment, creation_info.creator_comment)
+    add_literal_value(graph, creation_info_node, spdx_namespace.licenseListVersion, creation_info.license_list_version)
+    add_literal_value(graph, creation_info_node, RDFS.comment, creation_info.creator_comment)
 
     graph.add((doc_node, spdx_namespace.creationInfo, creation_info_node))
 
