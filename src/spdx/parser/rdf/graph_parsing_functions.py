659b2f712e8725b49b30544cc988b4053077ecce
Meret Behrens <meret.behrens@tngtech.com>
2023-02-01 14:36:16 +0100

    [issue-456] add graph parsing functions
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx/parser/rdf/graph_parsing_functions.py
+++ src/spdx/parser/rdf/graph_parsing_functions.py
@@ -8,13 +8,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, Callable
+from typing import Any, Callable, Union, Optional
 
-from rdflib import Graph
+from rdflib import Graph, URIRef
 from rdflib.exceptions import UniquenessError
 from rdflib.term import Node
 
+from spdx.model.spdx_no_assertion import SpdxNoAssertion, SPDX_NO_ASSERTION_STRING
+from spdx.model.spdx_none import SpdxNone, SPDX_NONE_STRING
 from spdx.parser.logger import Logger
+from spdx.rdfschema.namespace import SPDX_NAMESPACE
 
 
 def parse_literal(logger: Logger, graph: Graph, subject: Node, predicate: Node, default: Any = None,
@@ -29,3 +32,37 @@ def parse_literal(logger: Logger, graph: Graph, subject: Node, predicate: Node, 
         return method_to_apply(value.removeprefix(prefix))
 
     return default
+
+
+def parse_literal_or_no_assertion_or_none(logger: Logger, graph: Graph, subject: Node, predicate: Node,
+                                          default: Any = None, method_to_apply: Callable = lambda x: x):
+    try:
+        value = graph.value(subject=subject, predicate=predicate, default=default, any=False)
+    except UniquenessError:
+        logger.append(f"Multiple values for unique value {predicate} found.")
+        return
+    if not value:
+        return default
+    if value == SPDX_NAMESPACE.noassertion:
+        return SpdxNoAssertion()
+    if value == SPDX_NAMESPACE.none:
+        return SpdxNone()
+    return method_to_apply(value.toPython())
+
+
+def str_to_no_assertion_or_none(value: str) -> Union[str, SpdxNone, SpdxNoAssertion]:
+    if value == SPDX_NO_ASSERTION_STRING:
+        return SpdxNoAssertion()
+    if value == SPDX_NONE_STRING:
+        return SpdxNone()
+    return value
+
+
+def parse_spdx_id(resource: URIRef, doc_namespace: str) -> Optional[str]:
+    if not resource:
+        return None
+    if resource.startswith(f"{doc_namespace}#"):
+        spdx_id = resource.fragment
+    else:
+        spdx_id = resource.toPython()
+    return spdx_id or None
