f34bd1a61d2698e907d1989093129327bd0530b7
Armin Tänzer <armin.taenzer@tngtech.com>
2023-02-01 16:25:39 +0100

    [issue-386] test uppercase checksums/verification codes
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- tests/spdx/validation/test_package_verification_code_validator.py
+++ tests/spdx/validation/test_package_verification_code_validator.py
@@ -26,6 +26,8 @@ def test_valid_package_verification_code():
 @pytest.mark.parametrize("code, expected_message",
                          [(PackageVerificationCode("71c4025dd9897b364f3ebbb42c484ff43d00791cab", []),
                            "value of verification_code must consist of 40 lowercase hexadecimal digits, but is: 71c4025dd9897b364f3ebbb42c484ff43d00791cab (length: 42 digits)"),
+                          (PackageVerificationCode("CE9F343C4BA371746FD7EAD9B59031AE34D8AFC4", []),
+                           "value of verification_code must consist of 40 lowercase hexadecimal digits, but is: CE9F343C4BA371746FD7EAD9B59031AE34D8AFC4 (length: 40 digits)"),
                           (PackageVerificationCode("71c4025dd9897b364f3ebbb42c484ff43d00791c", ["/invalid/excluded/file"]),
                            'file name must not be an absolute path starting with "/", but is: /invalid/excluded/file')
                           ])
