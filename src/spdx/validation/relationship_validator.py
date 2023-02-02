d83275dc7a7c8cdda5a969d32747f92d7fdf403e
Armin Tänzer <armin.taenzer@tngtech.com>
2023-02-02 12:19:09 +0100

    [issue-463] fix usage of spdx version during validation
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- src/spdx/validation/relationship_validator.py
+++ src/spdx/validation/relationship_validator.py
@@ -19,15 +19,15 @@ from spdx.validation.spdx_id_validators import validate_spdx_id
 from spdx.validation.validation_message import ValidationMessage, ValidationContext, SpdxElementType
 
 
-def validate_relationships(relationships: List[Relationship], document: Document, spdx_version: str) -> List[ValidationMessage]:
+def validate_relationships(relationships: List[Relationship], spdx_version: str, document: Document) -> List[ValidationMessage]:
     validation_messages = []
     for relationship in relationships:
-        validation_messages.extend(validate_relationship(relationship, document, spdx_version))
+        validation_messages.extend(validate_relationship(relationship, spdx_version, document))
 
     return validation_messages
 
 
-def validate_relationship(relationship: Relationship, document: Document, spdx_version: str) -> List[ValidationMessage]:
+def validate_relationship(relationship: Relationship, spdx_version: str, document: Document) -> List[ValidationMessage]:
     validation_messages = []
     context = ValidationContext(element_type=SpdxElementType.RELATIONSHIP,
                                 full_element=relationship)
@@ -43,9 +43,9 @@ def validate_relationship(relationship: Relationship, document: Document, spdx_v
         for message in messages:
             validation_messages.append(ValidationMessage(message, context))
 
-    if spdx_version != "SPDX-2.3":
+    if spdx_version == "SPDX-2.2":
         if relationship_type == RelationshipType.SPECIFICATION_FOR or relationship_type == RelationshipType.REQUIREMENT_DESCRIPTION_FOR:
             validation_messages.append(
-                ValidationMessage(f"{relationship_type} is not supported for SPDX versions below SPDX-2.3", context))
+                ValidationMessage(f"{relationship_type} is not supported in SPDX-2.2", context))
 
     return validation_messages
