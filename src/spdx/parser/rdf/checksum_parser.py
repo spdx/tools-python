fb105dc26456c82d9f6800ba1bce2b3397bd2701
Meret Behrens <meret.behrens@tngtech.com>
2023-02-08 13:29:31 +0100

    [issue-456] use helper method for checksum algorithm
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx/parser/rdf/checksum_parser.py
+++ src/spdx/parser/rdf/checksum_parser.py
@@ -20,12 +20,8 @@ from spdx.rdfschema.namespace import SPDX_NAMESPACE
 
 def parse_checksum(parent_node: URIRef, graph: Graph) -> Checksum:
     logger = Logger()
-    algorithm = graph.value(parent_node, SPDX_NAMESPACE.algorithm, default="")
-    try:
-        algorithm = convert_rdf_to_algorithm(algorithm)
-    except KeyError:
-        logger.append(f"Invalid ChecksumAlgorithm: {algorithm}")
-        algorithm = None
+    algorithm = parse_literal(logger, graph, parent_node, SPDX_NAMESPACE.algorithm,
+                              method_to_apply=convert_rdf_to_algorithm)
     value = parse_literal(logger, graph, parent_node, SPDX_NAMESPACE.checksumValue)
     raise_parsing_error_if_logger_has_messages(logger, "Checksum")
     checksum = construct_or_raise_parsing_error(Checksum, dict(algorithm=algorithm, value=value))
