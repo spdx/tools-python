f7e6098c0223b66748386e087d48686387416bc3
Meret Behrens <meret.behrens@tngtech.com>
2023-02-10 08:34:27 +0100

    [spdx-3.0] allow only one subject per annotation
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- tests/spdx3/model/test_annotation.py
+++ tests/spdx3/model/test_annotation.py
@@ -17,13 +17,13 @@ from spdx3.model.annotation import Annotation, AnnotationType
 
 @mock.patch("spdx3.model.creation_information.CreationInformation", autospec=True)
 def test_correct_initialization(creation_information):
-    annotation = Annotation("SPDXRef-Annotation", creation_information, AnnotationType.OTHER, ["spdx_id1"],
+    annotation = Annotation("SPDXRef-Annotation", creation_information, AnnotationType.OTHER, "spdx_id1",
                             content_type="mediaType", statement="This is a statement")
 
     assert annotation.spdx_id == "SPDXRef-Annotation"
     assert annotation.creation_info == creation_information
     assert annotation.annotation_type == AnnotationType.OTHER
-    assert annotation.subject == ["spdx_id1"]
+    assert annotation.subject == "spdx_id1"
     assert annotation.content_type == "mediaType"
     assert annotation.statement == "This is a statement"
 
@@ -36,7 +36,7 @@ def test_invalid_initialization(creation_information):
 
     assert err.value.args[0] == ['SetterError Annotation: type of argument "annotation_type" must be '
                                  'spdx3.model.annotation.AnnotationType; got str instead: REVIEW',
-                                 'SetterError Annotation: type of argument "subject" must be a list; got dict '
+                                 'SetterError Annotation: type of argument "subject" must be str; got dict '
                                  "instead: {'element': 1}",
                                  'SetterError Annotation: type of argument "content_type" must be one of (str, '
                                  'NoneType); got int instead: 4',
