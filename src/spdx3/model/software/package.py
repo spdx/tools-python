f5f1551fd264a6609f98c95957b7531c1bf8a738
Meret Behrens <meret.behrens@tngtech.com>
2023-02-10 08:58:10 +0100

    [spdx-3.0] add ExternalIdentifier
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx3/model/software/package.py
+++ src/spdx3/model/software/package.py
@@ -10,6 +10,7 @@
 # limitations under the License.
 from typing import Optional, List
 
+from spdx3.model.external_identifier import ExternalIdentifier
 from spdx3.model.external_reference import ExternalReference
 
 from spdx3.model.creation_information import CreationInformation
@@ -33,11 +34,13 @@ class Package(Artifact):
     def __init__(self, spdx_id: str, creation_info: CreationInformation, name: Optional[str] = None,
                  summary: Optional[str] = None, description: Optional[str] = None, comment: Optional[str] = None,
                  verified_using: Optional[List[IntegrityMethod]] = None,
-                 external_references: Optional[List[ExternalReference]] = None, external_identifier: None = None,
-                 extension: None = None, originated_by: None = None, content_identifier: Optional[str] = None,
+                 external_references: Optional[List[ExternalReference]] = None,
+                 external_identifier: Optional[List[ExternalIdentifier]] = None, extension: None = None,
+                 originated_by: None = None, content_identifier: Optional[str] = None,
                  package_purpose: Optional[List[SoftwarePurpose]] = None, download_location: Optional[str] = None,
                  package_uri: Optional[str] = None, homepage: Optional[str] = None):
         verified_using = [] if verified_using is None else verified_using
         external_references = [] if external_references is None else external_references
+        external_identifier = [] if external_identifier is None else external_identifier
         package_purpose = [] if package_purpose is None else package_purpose
         check_types_and_set_values(self, locals())
