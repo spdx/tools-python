956cf637ec903cb616b455fdddb1c9f84b384111
Armin Tänzer <armin.taenzer@tngtech.com>
2023-02-24 12:29:05 +0100

    [issue-441] replace print() with logging
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- src/spdx/clitools/pyspdxtools.py
+++ src/spdx/clitools/pyspdxtools.py
@@ -10,6 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import logging
 import sys
 from typing import List
 
@@ -26,8 +27,11 @@ from spdx.writer.write_anything import write_file
 
 @click.command()
 @click.option("--infile", "-i", help="The file containing the document to be validated or converted.")
-@click.option("--outfile", "-o", help="The file to write the converted document to (write a dash for output to stdout or omit for no conversion).")
-@click.option("--version", help='The SPDX version to be used during parsing and validation ("SPDX-2.2" or "SPDX-2.3"). Will be read from the document if not provided.', default=None)
+@click.option("--outfile", "-o",
+              help="The file to write the converted document to (write a dash for output to stdout or omit for no conversion).")
+@click.option("--version",
+              help='The SPDX version to be used during parsing and validation ("SPDX-2.2" or "SPDX-2.3"). Will be read from the document if not provided.',
+              default=None)
 @click.option("--novalidation", is_flag=True, help="Don't validate the provided document.")
 def main(infile: str, outfile: str, version: str, novalidation: bool):
     """
@@ -46,34 +50,34 @@ def main(infile: str, outfile: str, version: str, novalidation: bool):
                 version = document.creation_info.spdx_version
 
             if not version in ["SPDX-2.2", "SPDX-2.3"]:
-                print(f"This tool only supports SPDX versions SPDX-2.2 and SPDX-2.3, but got: {version}",
-                      file=sys.stderr)
+                logging.error(f"This tool only supports SPDX versions SPDX-2.2 and SPDX-2.3, but got: {version}")
                 sys.exit(1)
 
             validation_messages: List[ValidationMessage] = validate_full_spdx_document(document, version)
             if validation_messages:
-                print("The document is invalid. The following issues have been found:", file=sys.stderr)
-                for message in validation_messages:
-                    print(message.validation_message, file=sys.stderr)
+                log_string = "\n".join(
+                    ["The document is invalid. The following issues have been found:"] +
+                    [message.validation_message for message in validation_messages])
+                logging.error(log_string)
                 sys.exit(1)
             else:
-                print("The document is valid.", file=sys.stderr)
+                logging.info("The document is valid.")
 
         if outfile and outfile != "-":
             write_file(document, outfile, validate=False)
 
     except NotImplementedError as err:
-        print(err.args[0], file=sys.stderr)
-        print("Please note that this project is currently undergoing a major refactoring and therefore missing "
-              "a few features which will be added in time (refer to https://github.com/spdx/tools-python/issues "
-              "for insights into the current status).\n"
-              "In the meantime, please use the PyPI release version 0.7.0.", file=sys.stderr)
+        logging.error(err.args[0] +
+                          "\nPlease note that this project is currently undergoing a major refactoring and therefore missing "
+                          "a few features which will be added in time (refer to https://github.com/spdx/tools-python/issues "
+                          "for insights into the current status).\n"
+                          "In the meantime, please use the current PyPI release version.")
         sys.exit(1)
 
     except SPDXParsingError as err:
-        print("There have been issues while parsing the provided document:", file=sys.stderr)
-        for message in err.get_messages():
-            print(message, file=sys.stderr)
+        log_string = "\n".join(["There have been issues while parsing the provided document:"] +
+                               [message for message in err.get_messages()])
+        logging.error(log_string)
         sys.exit(1)
 
 
