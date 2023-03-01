d186f4841775c8f43dd6b8502a56a50eb6603500
Meret Behrens <meret.behrens@tngtech.com>
2023-03-01 11:25:19 +0100

    [issue-382] add negative tests for extracted_licensing_info
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- tests/spdx/parser/tagvalue/test_extracted_licensing_info_parser.py
+++ tests/spdx/parser/tagvalue/test_extracted_licensing_info_parser.py
@@ -8,6 +8,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from unittest import TestCase
+
+import pytest
+
+from spdx.parser.error import SPDXParsingError
 from spdx.parser.tagvalue.parser.tagvalue import Parser
 from tests.spdx.parser.tagvalue.test_creation_info_parser import DOCUMENT_STR
 
@@ -16,9 +21,12 @@ def test_extracted_licensing_info():
     parser = Parser()
     extracted_licensing_info_str = '\n'.join([
         'LicenseID: LicenseRef-Beerware-4.2',
-        'ExtractedText: <text>"THE BEER-WARE LICENSE" (Revision 42): phk@FreeBSD.ORG wrote this file. As long as you retain this notice you can do whatever you want with this stuff. If we meet some day, and you think this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp</text>'
+        'ExtractedText: <text>"THE BEER-WARE LICENSE" (Revision 42): phk@FreeBSD.ORG wrote this file. As long as you '
+        'retain this notice you can do whatever you want with this stuff. If we meet some day, and you think this '
+        'stuff is worth it, you can buy me a beer in return Poul-Henning Kamp</text>'
         'LicenseName: Beer-Ware License (Version 42)',
         'LicenseCrossReference:  http://people.freebsd.org/~phk/',
+        'LicenseCrossReference:  http://another.cross.reference/',
         'LicenseComment: The beerware license has a couple of other standard variants.'
     ])
     document = parser.parse("\n".join([DOCUMENT_STR, extracted_licensing_info_str]))
@@ -26,7 +34,31 @@ def test_extracted_licensing_info():
     assert len(document.extracted_licensing_info) == 1
     extracted_licensing_info = document.extracted_licensing_info[0]
     assert extracted_licensing_info.license_id == "LicenseRef-Beerware-4.2"
-    assert extracted_licensing_info.extracted_text == '"THE BEER-WARE LICENSE" (Revision 42): phk@FreeBSD.ORG wrote this file. As long as you retain this notice you can do whatever you want with this stuff. If we meet some day, and you think this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp'
+    assert extracted_licensing_info.extracted_text == '"THE BEER-WARE LICENSE" (Revision 42): phk@FreeBSD.ORG wrote this file. ' \
+                                                      'As long as you retain this notice you can do whatever you want with this stuff. ' \
+                                                      'If we meet some day, and you think this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp'
     assert extracted_licensing_info.license_name == "Beer-Ware License (Version 42)"
-    assert extracted_licensing_info.cross_references == ["http://people.freebsd.org/~phk/"]
+    TestCase().assertCountEqual(extracted_licensing_info.cross_references,
+                                ["http://people.freebsd.org/~phk/", "http://another.cross.reference/"])
     assert extracted_licensing_info.comment == "The beerware license has a couple of other standard variants."
+
+
+def test_parse_invalid_licensing_info():
+    parser = Parser()
+    extracted_licensing_info_str = '\n'.join([
+        'ExtractedText: <text>"THE BEER-WARE LICENSE" (Revision 42): phk@FreeBSD.ORG wrote this file. As long as you retain this notice you can do whatever you want with this stuff. If we meet some day, and you think this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp</text>'
+        'LicenseName: Beer-Ware License (Version 42)',
+        'LicenseCrossReference:  http://people.freebsd.org/~phk/',
+        'LicenseComment: The beerware license has a couple of other standard variants.'])
+
+    with pytest.raises(SPDXParsingError) as err:
+        parser.parse(extracted_licensing_info_str)
+
+    assert err.value.get_messages() == ['Element ExtractedLicensingInfo is not the current element in scope, probably '
+                                        'the expected tag to start the element (LicenseID) is missing.',
+                                        'Element ExtractedLicensingInfo is not the current element in scope, probably '
+                                        'the expected tag to start the element (LicenseID) is missing.',
+                                        'Element ExtractedLicensingInfo is not the current element in scope, probably '
+                                        'the expected tag to start the element (LicenseID) is missing.',
+                                        'Element ExtractedLicensingInfo is not the current element in scope, probably '
+                                        'the expected tag to start the element (LicenseID) is missing.']
