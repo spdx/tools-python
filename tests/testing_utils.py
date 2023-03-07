b0d946b354045ae4e01398f416ee7abbabfe8443
Marc-Etienne Vargenau <marc-etienne.vargenau@nokia.com>
2023-03-07 13:06:09 +0100

    Adding SPDX-License-Identifier in source code files.
    Fix #511
    
    Signed-off-by: Marc-Etienne Vargenau <marc-etienne.vargenau@nokia.com>

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -3,6 +3,7 @@
 # https://raw.githubusercontent.com/nose-devs/nose/7c26ad1e6b7d308cafa328ad34736d34028c122a/nose/tools/nontrivial.py
 # Copyright (c) 2005-2009 Jason Pellerin and others.
 #
+# SPDX-License-Identifier: GPL-2.0-or-later
 # This program is free software; you can redistribute it and/or modify it under the
 # terms of the GNU Lesser General Public License as published by the Free Software
 # Foundation; either version 2 of the License, or (at your option) any later version.
