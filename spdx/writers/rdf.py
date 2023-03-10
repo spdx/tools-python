de7c0d79a615b00993371cec5ef55c6dd8d1fd6a
Meret Behrens <meret.behrens@tngtech.com>
2023-03-10 16:34:46 +0100

    [issue-514] fix writing of multiple packages to rdf files
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- spdx/writers/rdf.py
+++ spdx/writers/rdf.py
@@ -793,18 +793,9 @@ class PackageWriter(LicenseWriter):
         Return a Node representing the package.
         Files must have been added to the graph before this method is called.
         """
-        package_node = URIRef("http://www.spdx.org/tools#SPDXRef-Package")
+        package_node = URIRef(f"http://www.spdx.org/tools#{package.spdx_id}")
         type_triple = (package_node, RDF.type, self.spdx_namespace.Package)
         self.graph.add(type_triple)
-        # Package SPDXID
-        if package.spdx_id:
-            pkg_spdx_id = URIRef(package.spdx_id)
-            pkg_spdx_id_triple = (
-                package_node,
-                self.spdx_namespace.Package,
-                pkg_spdx_id,
-            )
-            self.graph.add(pkg_spdx_id_triple)
         # Handle optional fields:
         self.handle_pkg_optional_fields(package, package_node)
         # package name
