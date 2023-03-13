06db03044ef3c8745c0443b7b3da299533686b0f
Armin Tänzer <armin.taenzer@tngtech.com>
2023-03-13 09:24:23 +0100

    [issue-426] add Tool and Agent-related classes
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>
    
    [spdx-3.0] add Agent
    
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>
    
    small change to model for agent
    
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>
    
    small change to model for agent
    
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>
    
    add tests to model
    
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

---
+++ tests/spdx3/model/test_agent.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2023 spdx contributors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from datetime import datetime
+
+import pytest
+from semantic_version import Version
+
+from spdx3.model.creation_information import CreationInformation
+from spdx3.model.external_identifier import ExternalIdentifier, ExternalIdentifierType
+from spdx3.model.organization import Organization
+from spdx3.model.person import Person
+from spdx3.model.software_agent import SoftwareAgent
+
+
+@pytest.mark.parametrize("agent_class", [Person, Organization, SoftwareAgent])
+def test_correct_initialization(agent_class):
+    agent = agent_class("SPDXRef-Agent",
+                        CreationInformation(Version("3.0.0"), datetime(2023, 1, 1), ["SPDXRef-Agent"], ["core"], "CC0"),
+                        external_identifier=[ExternalIdentifier(ExternalIdentifierType.EMAIL, "some@mail.com")])
+
+    assert agent.spdx_id == "SPDXRef-Agent"
+    assert agent.creation_info == CreationInformation(Version("3.0.0"), datetime(2023, 1, 1), ["SPDXRef-Agent"],
+                                                      ["core"], "CC0")
+    assert agent.external_identifier == [ExternalIdentifier(ExternalIdentifierType.EMAIL, "some@mail.com")]
