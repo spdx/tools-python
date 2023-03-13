b593d1c6d3f73b90cbffe70aa8ef380428d085ba
Armin Tänzer <armin.taenzer@tngtech.com>
2023-03-13 09:24:23 +0100

    [issue-426] add Tool and Agent-related classes
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- src/spdx3/model/tool.py
+++ src/spdx3/model/tool.py
@@ -2,7 +2,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#   http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -12,29 +12,23 @@ from typing import Optional, List
 
 from common.typing.dataclass_with_properties import dataclass_with_properties
 from common.typing.type_checks import check_types_and_set_values
-from spdx3.model.artifact import Artifact
 from spdx3.model.creation_information import CreationInformation
+from spdx3.model.element import Element
 from spdx3.model.external_identifier import ExternalIdentifier
 from spdx3.model.external_reference import ExternalReference
 from spdx3.model.integrity_method import IntegrityMethod
-from spdx3.model.software.software_purpose import SoftwarePurpose
 
 
 @dataclass_with_properties
-class File(Artifact):
-    content_identifier: Optional[str] = None  # should be a valid URI
-    file_purpose: Optional[List[SoftwarePurpose]] = None
-    content_type: Optional[str] = None  # placeholder for MediaType
+class Tool(Element):
 
-    def __init__(self, spdx_id: str, creation_info: CreationInformation, name: Optional[str] = None,
-                 summary: Optional[str] = None, description: Optional[str] = None, comment: Optional[str] = None,
+    def __init__(self, spdx_id: str, creation_info: CreationInformation,
+                 name: Optional[str] = None, summary: Optional[str] = None,
+                 description: Optional[str] = None, comment: Optional[str] = None,
                  verified_using: Optional[List[IntegrityMethod]] = None,
                  external_references: Optional[List[ExternalReference]] = None,
-                 external_identifier: Optional[List[ExternalIdentifier]] = None, extension: None = None,
-                 originated_by: Optional[str] = None, content_identifier: Optional[str] = None,
-                 file_purpose: Optional[List[SoftwarePurpose]] = None, content_type: Optional[str] = None):
+                 external_identifier: Optional[List[ExternalIdentifier]] = None, extension: None = None):
         verified_using = [] if verified_using is None else verified_using
         external_references = [] if external_references is None else external_references
         external_identifier = [] if external_identifier is None else external_identifier
-        file_purpose = [] if file_purpose is None else file_purpose
         check_types_and_set_values(self, locals())
