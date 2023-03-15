03c441a99186ff87594315c5e0e7124dc8636f2a
Armin Tänzer <armin.taenzer@tngtech.com>
2023-03-15 12:11:44 +0100

    [issue-427] adapt bump methods to allow bumping of actors within other elements
    also change spdx_id_map to payload
    
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>
    
    small change
    
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- src/spdx3/bump_from_spdx2/annotation.py
+++ src/spdx3/bump_from_spdx2/annotation.py
@@ -13,9 +13,11 @@ from spdx.model.annotation import Annotation as Spdx2_Annotation
 from spdx3.bump_from_spdx2.message import print_missing_conversion
 from spdx3.model.annotation import Annotation, AnnotationType
 from spdx3.model.creation_information import CreationInformation
+from spdx3.payload import Payload
 
 
-def bump_annotation(spdx2_annotation: Spdx2_Annotation, creation_info: CreationInformation, counter: int) -> Annotation:
+def bump_annotation(spdx2_annotation: Spdx2_Annotation, payload: Payload, creation_info: CreationInformation,
+                    counter: int):
     spdx_id: str = f"SPDXRef-Annotation-{counter}"
     creation_info.created = spdx2_annotation.annotation_date
     # creation_info.created_by = bump_actor(spdx2_annotation.annotator)   waiting for entity implementation
@@ -24,4 +26,4 @@ def bump_annotation(spdx2_annotation: Spdx2_Annotation, creation_info: CreationI
     subject: str = spdx2_annotation.spdx_id
     statement: str = spdx2_annotation.annotation_comment
 
-    return Annotation(spdx_id, creation_info, annotation_type, subject, statement=statement)
+    payload.add_element(Annotation(spdx_id, creation_info, annotation_type, subject, statement=statement))
