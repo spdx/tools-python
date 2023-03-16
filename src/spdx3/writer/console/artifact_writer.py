70ffc6731b1a5c202d19e363bcd7dd607ff3e1f0
Armin Tänzer <armin.taenzer@tngtech.com>
2023-03-16 12:22:21 +0100

    [issue-434] add writing of Agents/Tool (except for supplier)
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- src/spdx3/writer/console/artifact_writer.py
+++ src/spdx3/writer/console/artifact_writer.py
@@ -11,9 +11,10 @@
 from typing import TextIO
 
 from spdx3.model.artifact import Artifact
+from spdx3.writer.console.console import write_value
 from spdx3.writer.console.element_writer import write_element_properties
 
 
 def write_artifact_properties(artifact: Artifact, text_output: TextIO):
     write_element_properties(artifact, text_output)
-    # write_value("originated_by", artifact.originated_by) not implemented yet
+    write_value("originated_by", artifact.originated_by, text_output)
