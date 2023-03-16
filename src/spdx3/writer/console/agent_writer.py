cdb87ce5ca167364a26a4e1a0734de76de31ac29
Armin Tänzer <armin.taenzer@tngtech.com>
2023-03-16 12:22:21 +0100

    [issue-434] add writing of Agents/Tool (except for supplier)
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

---
+++ src/spdx3/writer/console/agent_writer.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2023 spdx contributors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import TextIO
+
+from spdx3.model.agent import Agent
+from spdx3.model.organization import Organization
+from spdx3.model.person import Person
+from spdx3.model.software_agent import SoftwareAgent
+from spdx3.writer.console.element_writer import write_element_properties
+
+
+def write_agent(agent: Agent, text_output: TextIO, heading: bool = True):
+    if heading:
+        if isinstance(agent, Person):
+            text_output.write("## Person\n")
+        if isinstance(agent, Organization):
+            text_output.write("## Organization\n")
+        if isinstance(agent, SoftwareAgent):
+            text_output.write("## SoftwareAgent\n")
+    write_element_properties(agent, text_output)
