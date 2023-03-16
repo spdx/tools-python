959531ce6ba6ceb89281de6f2b1abc2cfc9141d5
Armin Tänzer <armin.taenzer@tngtech.com>
2023-03-16 12:21:34 +0100

    [issue-427] add bumping of actors (except for supplier)
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- src/spdx3/bump_from_spdx2/creation_information.py
+++ src/spdx3/bump_from_spdx2/creation_information.py
@@ -9,17 +9,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from datetime import datetime
+from typing import List
 
 from semantic_version import Version
+from spdx3.payload import Payload
 
 from spdx.model.document import CreationInfo as Spdx2_CreationInfo
+from spdx3.bump_from_spdx2.actor import bump_actor
 from spdx3.bump_from_spdx2.external_document_ref import bump_external_document_ref
 from spdx3.bump_from_spdx2.message import print_missing_conversion
 from spdx3.model.creation_information import CreationInformation
 from spdx3.model.spdx_document import SpdxDocument
 
 
-def bump_creation_information(spdx2_creation_info: Spdx2_CreationInfo) -> SpdxDocument:
+def bump_creation_information(spdx2_creation_info: Spdx2_CreationInfo, payload: Payload) -> SpdxDocument:
     # creation_info.spdx_id -> spdx_document.spdx_id
     spdx_id = spdx2_creation_info.spdx_id
 
@@ -28,8 +31,7 @@ def bump_creation_information(spdx2_creation_info: Spdx2_CreationInfo) -> SpdxDo
 
     # creation_info.document_namespace -> ?
     print_missing_conversion("creation_info.document_namespace", 0)
-    # creation_info.creators -> creation_information.creators (not implemented yet)
-    print_missing_conversion("creation_info.creators", 1, "of creators")
+
     created: datetime = spdx2_creation_info.created
     # creation_info.creator_comment -> ?
     print_missing_conversion("creation_info.creator_comment", 0)
@@ -41,8 +43,16 @@ def bump_creation_information(spdx2_creation_info: Spdx2_CreationInfo) -> SpdxDo
     print_missing_conversion("creation_info.license_list_version", 0)
     # creation_info.document_comment -> spdx_document.comment
     document_comment = spdx2_creation_info.document_comment
-    creation_information = CreationInformation(Version("3.0.0"), created, None, ["core", "software", "licensing"],
+    creation_information = CreationInformation(Version("3.0.0"), created, [], ["core", "software", "licensing"],
                                                data_license)
+
+    # due to creators having a creation_information themselves which inherits from the document's one,
+    # we have to add them after the creation_information has been initialized
+    creator_ids: List[str] = []
+    for creator in spdx2_creation_info.creators:
+        creator_ids.append(bump_actor(creator, payload, creation_information))
+    creation_information.created_by = creator_ids
+
     spdx_document = SpdxDocument(spdx_id=spdx_id, creation_info=creation_information, name=name,
                                  comment=document_comment, elements=[], root_elements=[], imports=imports)
 
