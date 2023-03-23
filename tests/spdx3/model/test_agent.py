3153b6b1a0d3e1a545fe8d408b042fd1f3d44e3a
Armin Tänzer <armin.taenzer@tngtech.com>
2023-03-23 11:51:17 +0100

    [review] add test_invalid_initialization for agent classes
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- tests/spdx3/model/test_agent.py
+++ tests/spdx3/model/test_agent.py
@@ -13,6 +13,7 @@ from datetime import datetime
 import pytest
 from semantic_version import Version
 
+from spdx3.model.agent import Agent
 from spdx3.model.creation_information import CreationInformation
 from spdx3.model.external_identifier import ExternalIdentifier, ExternalIdentifierType
 from spdx3.model.organization import Organization
@@ -20,7 +21,7 @@ from spdx3.model.person import Person
 from spdx3.model.software_agent import SoftwareAgent
 
 
-@pytest.mark.parametrize("agent_class", [Person, Organization, SoftwareAgent])
+@pytest.mark.parametrize("agent_class", [Agent, Person, Organization, SoftwareAgent])
 def test_correct_initialization(agent_class):
     agent = agent_class("SPDXRef-Agent",
                         CreationInformation(Version("3.0.0"), datetime(2023, 1, 1), ["SPDXRef-Agent"], ["core"], "CC0"),
@@ -30,3 +31,13 @@ def test_correct_initialization(agent_class):
     assert agent.creation_info == CreationInformation(Version("3.0.0"), datetime(2023, 1, 1), ["SPDXRef-Agent"],
                                                       ["core"], "CC0")
     assert agent.external_identifier == [ExternalIdentifier(ExternalIdentifierType.EMAIL, "some@mail.com")]
+
+
+@pytest.mark.parametrize("agent_class", [Agent, Person, Organization, SoftwareAgent])
+def test_invalid_initialization(agent_class):
+    with pytest.raises(TypeError) as err:
+        agent_class(12, 345)
+
+    assert err.value.args[0] == [f'SetterError {agent_class.__name__}: type of argument "spdx_id" must be str; got int instead: 12',
+                                 f'SetterError {agent_class.__name__}: type of argument "creation_info" must be '
+                                 'spdx3.model.creation_information.CreationInformation; got int instead: 345']
