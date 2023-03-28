4971c900f01f7457e280282b630db27a3adfaa8f
Armin Tänzer <armin.taenzer@tngtech.com>
2023-03-28 14:53:51 +0200

    [spdx-3.0] add created_using to CreationInformation
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- tests/spdx3/model/test_creation_information.py
+++ tests/spdx3/model/test_creation_information.py
@@ -17,19 +17,20 @@ from spdx3.model.creation_information import CreationInformation
 
 
 def test_correct_initialization():
-    creation_information = CreationInformation(Version("3.0.0"), datetime(2023, 1, 11, 16, 21), [],
+    creation_information = CreationInformation(Version("3.0.0"), datetime(2023, 1, 11, 16, 21), [], [],
                                                ["core", "software"], "CC0")
 
     assert creation_information.spec_version == Version("3.0.0")
     assert creation_information.created == datetime(2023, 1, 11, 16, 21)
     assert creation_information.created_by == []
+    assert creation_information.created_using == []
     assert creation_information.profile == ["core", "software"]
     assert creation_information.data_license == "CC0"
 
 
 def test_invalid_initialization():
     with pytest.raises(TypeError) as err:
-        CreationInformation("2.3", "2012-01-01", [], "core", 3)
+        CreationInformation("2.3", "2012-01-01", [], [], "core", 3)
 
     assert err.value.args[0] == ['SetterError CreationInformation: type of argument "spec_version" must be '
                                  'semantic_version.base.Version; got str instead: 2.3',
@@ -45,5 +46,5 @@ def test_incomplete_initialization():
     with pytest.raises(TypeError) as err:
         CreationInformation("2.3")
 
-    assert "__init__() missing 3 required positional arguments: 'created', 'created_by', and 'profile'" in \
+    assert "__init__() missing 4 required positional arguments: 'created', 'created_by', 'created_using', and 'profile'" in \
            err.value.args[0]
