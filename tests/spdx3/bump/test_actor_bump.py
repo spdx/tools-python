e46767ec59aa5d8d6c6cc2a2e0cdfa770e0c710a
Armin Tänzer <armin.taenzer@tngtech.com>
2023-03-28 15:46:43 +0200

    [review] enhance test_actor_bump.py
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- tests/spdx3/bump/test_actor_bump.py
+++ tests/spdx3/bump/test_actor_bump.py
@@ -16,6 +16,7 @@ from semantic_version import Version
 from spdx.model.actor import Actor, ActorType
 from spdx3.bump_from_spdx2.actor import bump_actor
 from spdx3.model.creation_information import CreationInformation
+from spdx3.model.external_identifier import ExternalIdentifier, ExternalIdentifierType
 from spdx3.model.organization import Organization
 from spdx3.model.person import Person
 from spdx3.model.tool import Tool
@@ -40,9 +41,14 @@ def test_bump_actor(actor_type, actor_name, actor_mail, element_type, new_spdx_i
     assert isinstance(agent_or_tool, element_type)
     assert agent_or_tool.spdx_id == new_spdx_id
     assert agent_or_tool.name == actor_name
+    if actor_mail:
+        assert len(agent_or_tool.external_identifier) == 1
+        assert agent_or_tool.external_identifier[0] == ExternalIdentifier(ExternalIdentifierType.EMAIL, actor_mail)
+    else:
+        assert len(agent_or_tool.external_identifier) == 0
 
 
-def test_bump_actor_with_already_existing_actor():
+def test_bump_actor_that_already_exists():
     creation_info_old = CreationInformation(Version("3.0.0"), datetime(2022, 1, 1), ["Creator"], [], ["core"])
     creation_info_new = CreationInformation(Version("3.0.0"), datetime(2023, 2, 2), ["Creator"], [], ["core"])
 
@@ -53,4 +59,7 @@ def test_bump_actor_with_already_existing_actor():
     actor = Actor(ActorType.PERSON, name, "some@mail.com")
     agent_spdx_id = bump_actor(actor, payload, creation_info_new)
 
+    # assert that there is only one Person in the payload
+    assert len([payload.get_element(person_id) for person_id in payload.get_full_map() if
+                isinstance(payload.get_element(person_id), Person)]) == 1
     assert payload.get_element(agent_spdx_id).creation_info == creation_info_old
