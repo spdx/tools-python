0e2220479573b99d27d648c31213a2e8b2b4aedc
Meret Behrens <meret.behrens@tngtech.com>
2023-04-04 10:16:43 +0200

    [issue-561] fix rdf parser: add logic to parse implicit "hasFiles" and "describesPackage" relationships
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx/parser/rdf/rdf_parser.py
+++ src/spdx/parser/rdf/rdf_parser.py
@@ -1,9 +1,12 @@
 # SPDX-FileCopyrightText: 2023 spdx contributors
 #
 # SPDX-License-Identifier: Apache-2.0
+from typing import Any, Dict
+
 from rdflib import RDF, Graph
 
 from spdx.model.document import Document
+from spdx.model.relationship import RelationshipType
 from spdx.parser.error import SPDXParsingError
 from spdx.parser.logger import Logger
 from spdx.parser.parsing_functions import construct_or_raise_parsing_error, raise_parsing_error_if_logger_has_messages
@@ -12,7 +15,7 @@ from spdx.parser.rdf.creation_info_parser import parse_creation_info
 from spdx.parser.rdf.extracted_licensing_info_parser import parse_extracted_licensing_info
 from spdx.parser.rdf.file_parser import parse_file
 from spdx.parser.rdf.package_parser import parse_package
-from spdx.parser.rdf.relationship_parser import parse_relationship
+from spdx.parser.rdf.relationship_parser import parse_implicit_relationship, parse_relationship
 from spdx.parser.rdf.snippet_parser import parse_snippet
 from spdx.rdfschema.namespace import SPDX_NAMESPACE
 
@@ -27,7 +30,7 @@ def parse_from_file(file_name: str) -> Document:
 
 
 def translate_graph_to_document(graph: Graph) -> Document:
-    parsed_fields = dict()
+    parsed_fields: Dict[str, Any] = dict()
     logger = Logger()
     try:
         creation_info, doc_node = parse_creation_info(graph)
@@ -61,6 +64,21 @@ def translate_graph_to_document(graph: Graph) -> Document:
             except SPDXParsingError as err:
                 logger.extend(err.get_messages())
         parsed_fields[element] = elements
+
+    for triple, relationship_type in [
+        ((None, SPDX_NAMESPACE.hasFile, None), RelationshipType.CONTAINS),
+        ((None, SPDX_NAMESPACE.describesPackage, None), RelationshipType.DESCRIBES),
+    ]:
+        for parent_node, _, element_node in graph.triples(triple):
+            try:
+                relationship = parse_implicit_relationship(
+                    parent_node, relationship_type, element_node, graph, creation_info.document_namespace
+                )
+                if relationship not in parsed_fields["relationships"]:
+                    parsed_fields["relationships"].append(relationship)
+
+            except SPDXParsingError as err:
+                logger.extend(err.get_messages())
 
     extracted_licensing_infos = []
     for _, _, extracted_licensing_info_node in graph.triples((None, SPDX_NAMESPACE.hasExtractedLicensingInfo, None)):
--- tests/spdx/parser/rdf/test_relationship_parser.py
+++ tests/spdx/parser/rdf/test_relationship_parser.py
@@ -3,10 +3,11 @@
 # SPDX-License-Identifier: Apache-2.0
 import os
 
-from rdflib import RDF, Graph
+import pytest
+from rdflib import RDF, Graph, URIRef
 
 from spdx.model.relationship import RelationshipType
-from spdx.parser.rdf.relationship_parser import parse_relationship
+from spdx.parser.rdf.relationship_parser import parse_implicit_relationship, parse_relationship
 from spdx.rdfschema.namespace import SPDX_NAMESPACE
 
 
@@ -22,3 +23,34 @@ def test_relationship_parser():
     assert relationship.relationship_type == RelationshipType.DESCRIBES
     assert relationship.related_spdx_element_id == "SPDXRef-File"
     assert relationship.comment == "relationshipComment"
+
+
+@pytest.mark.parametrize(
+    "parent_node, predicate, spdx_element_id, relationship_type, related_spdx_element_id",
+    [
+        (
+            SPDX_NAMESPACE.SpdxDocument,
+            SPDX_NAMESPACE.describesPackage,
+            "SPDXRef-DOCUMENT",
+            RelationshipType.DESCRIBES,
+            "SPDXRef-Package",
+        ),
+        (SPDX_NAMESPACE.Package, SPDX_NAMESPACE.hasFile, "SPDXRef-Package", RelationshipType.CONTAINS, "SPDXRef-File"),
+    ],
+)
+def test_parse_implicit_relationship(
+    parent_node, predicate, spdx_element_id, relationship_type, related_spdx_element_id
+):
+    graph = Graph().parse(os.path.join(os.path.dirname(__file__), "data/file_to_test_rdf_parser.rdf.xml"))
+    parent_node = graph.value(predicate=RDF.type, object=parent_node)
+    relationship_node = graph.value(subject=parent_node, predicate=predicate)
+    assert isinstance(relationship_node, URIRef)
+    assert isinstance(parent_node, URIRef)
+
+    doc_namespace = "https://some.namespace"
+
+    relationship = parse_implicit_relationship(parent_node, relationship_type, relationship_node, graph, doc_namespace)
+
+    assert relationship.spdx_element_id == spdx_element_id
+    assert relationship.relationship_type == relationship_type
+    assert relationship.related_spdx_element_id == related_spdx_element_id
