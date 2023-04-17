635695ebf16b96818d5c925d6c549512656e7743
Meret Behrens <meret.behrens@tngtech.com>
2023-04-17 14:02:58 +0200

    [issue_586] add tests for cli usage
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

---
+++ tests/spdx/test_cli.py
@@ -0,0 +1,45 @@
+import os
+
+import pytest
+from click.testing import CliRunner
+
+from spdx.clitools.pyspdxtools import main
+
+
+@pytest.mark.parametrize(
+    "options",
+    [
+        ("--infile", os.path.join(os.path.dirname(__file__), "data/SPDXJSONExample-v2.3.spdx.json")),
+        ("-i", os.path.join(os.path.dirname(__file__), "data/SPDXJSONExample-v2.3.spdx.json"), "--novalidation"),
+        (
+            "-i",
+            os.path.join(os.path.dirname(__file__), "data/SPDXJSONExample-v2.3.spdx.json"),
+            "--novalidation",
+            "--version",
+            "SPDX-2.3",
+        ),
+        ("-i", os.path.join(os.path.dirname(__file__), "data/SPDXJSONExample-v2.3.spdx.json"), "-o", "-"),
+    ],
+)
+def test_cli_with_system_exit_code_0(options):
+    runner = CliRunner()
+
+    result = runner.invoke(main, options)
+
+    assert result.exit_code == 0
+
+
+@pytest.mark.parametrize(
+    "options",
+    [
+        (),
+        ("-i", os.path.join(os.path.dirname(__file__), "data/SPDXJSONExample-v2.3.spdx.json"), "--version"),
+        ("-i", os.path.join(os.path.dirname(__file__), "data/SPDXJSONExample-v2.3.spdx.json"), "-o"),
+    ],
+)
+def test_cli_with_system_exit_code_2(options):
+    runner = CliRunner()
+
+    result = runner.invoke(main, options)
+
+    assert result.exit_code == 2
