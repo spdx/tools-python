e606553d71f6a886f532d6d572728242d5355ab3
Armin Tänzer <armin.taenzer@tngtech.com>
2023-04-20 16:29:12 +0200

    [issue-426] add AIPackage class
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- src/spdx_tools/spdx3/writer/console/software/package_writer.py
+++ src/spdx_tools/spdx3/writer/console/software/package_writer.py
@@ -8,8 +8,9 @@ from spdx_tools.spdx3.writer.console.artifact_writer import write_artifact_prope
 from spdx_tools.spdx3.writer.console.console import write_value
 
 
-def write_package(package: Package, text_output: TextIO):
-    text_output.write("## Package\n")
+def write_package(package: Package, text_output: TextIO, heading: bool = True):
+    if heading:
+        text_output.write("## Package\n")
     write_artifact_properties(package, text_output)
     write_value("content_identifier", package.content_identifier, text_output)
     write_value("package_purpose", ", ".join([purpose.name for purpose in package.package_purpose]), text_output)
