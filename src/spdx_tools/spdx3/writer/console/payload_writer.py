0f314d0ce1fc95d4543e6f4c1f772a104955926c
Meret Behrens <meret.behrens@tngtech.com>
2023-04-26 13:20:05 +0200

    [issue_602] implement review comments
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx_tools/spdx3/writer/console/payload_writer.py
+++ src/spdx_tools/spdx3/writer/console/payload_writer.py
@@ -31,13 +31,16 @@ from spdx_tools.spdx3.writer.console.software.file_writer import write_file
 from spdx_tools.spdx3.writer.console.software.package_writer import write_package
 from spdx_tools.spdx3.writer.console.software.sbom_writer import write_sbom
 from spdx_tools.spdx3.writer.console.software.snippet_writer import write_snippet
+from spdx_tools.spdx3.writer.console.software.software_dependency_relationship_writer import (
+    write_software_dependency_relationship,
+)
 from spdx_tools.spdx3.writer.console.spdx_document_writer import write_spdx_document
 from spdx_tools.spdx3.writer.console.tool_writer import write_tool
 
 MAP_CLASS_TO_WRITE_METHOD = {
     Annotation: write_annotation,
     Relationship: write_relationship,
-    SoftwareDependencyRelationship: write_relationship, # needs to be adapted as soon as base PR is changed
+    SoftwareDependencyRelationship: write_software_dependency_relationship,
     Bundle: write_bundle,
     SpdxDocument: write_spdx_document,
     Bom: write_bom,
