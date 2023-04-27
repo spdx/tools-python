9b728214eb38e9f5e5bb34ec66ba8ee7dc56210f
Armin Tänzer <armin.taenzer@tngtech.com>
2023-04-27 14:33:02 +0200

    [issue-489] try to add pyshacl validation
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

---
+++ tests/spdx3/validation/json_ld/test_shacl_validation.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2023 spdx contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+from spdx_tools.spdx3.validation.json_ld.shacl_validation import validate_against_shacl_from_file
+
+
+def test_shacl_validation():
+    some_return = validate_against_shacl_from_file(
+        data_file="/home/armin/PycharmProjects/tools-python/tests/SPDX3_jsonld_test.json",
+        shacl_file="/home/armin/PycharmProjects/tools-python/src/spdx_tools/spdx3/writer/json_ld/model.ttl")
+
+    print(some_return)
