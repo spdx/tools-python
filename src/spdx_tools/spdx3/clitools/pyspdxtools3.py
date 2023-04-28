46bdf13fffb3e8c04fc9d44b5990ca2890c6a9bc
Meret Behrens <meret.behrens@tngtech.com>
2023-04-28 08:28:06 +0200

    add cli functionality to use json ld serialization
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx_tools/spdx3/clitools/pyspdxtools3.py
+++ src/spdx_tools/spdx3/clitools/pyspdxtools3.py
@@ -8,11 +8,12 @@ import click
 
 from spdx_tools.spdx3.bump_from_spdx2.spdx_document import bump_spdx_document
 from spdx_tools.spdx3.payload import Payload
-from spdx_tools.spdx3.writer.console.payload_writer import write_payload
+from spdx_tools.spdx3.writer.console.payload_writer import write_payload as write_payload_to_console
 from spdx_tools.spdx.model.document import Document
 from spdx_tools.spdx.parser.parse_anything import parse_file
 from spdx_tools.spdx.validation.document_validator import validate_full_spdx_document
 from spdx_tools.spdx.validation.validation_message import ValidationMessage
+from spdx_tools.spdx3.writer.json_ld.json_ld_writer import write_payload
 
 
 @click.command()
@@ -22,7 +23,9 @@ from spdx_tools.spdx.validation.validation_message import ValidationMessage
 @click.option(
     "--outfile",
     "-o",
-    help="The file to write the converted document to (write a dash for output to stdout or omit for no conversion).",
+    help="The file to write the converted document to (write a dash for output to stdout or omit for no conversion)."
+         "For now only a prototype serialization to json_ld is available. The provided file will therefore be extended "
+         "with `_jsonld.json`.",
 )
 @click.option(
     "--version",
@@ -48,10 +51,12 @@ def main(infile: str, outfile: str, version: str, novalidation: bool):
                 sys.exit(1)
             else:
                 print("The document is valid.", file=sys.stderr)
-
-        if outfile == "-":
+        if outfile:
             payload: Payload = bump_spdx_document(document)
-            write_payload(payload, sys.stdout)
+            if outfile == "-":
+                write_payload_to_console(payload, sys.stdout)
+            else:
+                write_payload(payload, outfile)
 
     except NotImplementedError as err:
         print(err.args[0])
