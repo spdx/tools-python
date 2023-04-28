bb7ec1437ef1b06ad21ecaf3c8655d0e09845336
Meret Behrens <meret.behrens@tngtech.com>
2023-04-28 08:39:31 +0200

    fix linting
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- tests/spdx3/validation/json_ld/test_shacl_validation.py
+++ tests/spdx3/validation/json_ld/test_shacl_validation.py
@@ -7,6 +7,7 @@ from spdx_tools.spdx3.validation.json_ld.shacl_validation import validate_agains
 def test_shacl_validation():
     some_return = validate_against_shacl_from_file(
         data_file="/home/armin/PycharmProjects/tools-python/tests/SPDX3_jsonld_test.json",
-        shacl_file="/home/armin/PycharmProjects/tools-python/src/spdx_tools/spdx3/writer/json_ld/model.ttl")
+        shacl_file="/home/armin/PycharmProjects/tools-python/src/spdx_tools/spdx3/writer/json_ld/model.ttl",
+    )
 
     print(some_return)
