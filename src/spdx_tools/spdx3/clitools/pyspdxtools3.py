f26b9fc9af8aac04789d6cd681c2645c0e5a96fe
Meret Behrens <meret.behrens@tngtech.com>
2023-04-28 08:39:31 +0200

    fix linting
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx_tools/spdx3/clitools/pyspdxtools3.py
+++ src/spdx_tools/spdx3/clitools/pyspdxtools3.py
@@ -9,11 +9,11 @@ import click
 from spdx_tools.spdx3.bump_from_spdx2.spdx_document import bump_spdx_document
 from spdx_tools.spdx3.payload import Payload
 from spdx_tools.spdx3.writer.console.payload_writer import write_payload as write_payload_to_console
+from spdx_tools.spdx3.writer.json_ld.json_ld_writer import write_payload
 from spdx_tools.spdx.model.document import Document
 from spdx_tools.spdx.parser.parse_anything import parse_file
 from spdx_tools.spdx.validation.document_validator import validate_full_spdx_document
 from spdx_tools.spdx.validation.validation_message import ValidationMessage
-from spdx_tools.spdx3.writer.json_ld.json_ld_writer import write_payload
 
 
 @click.command()
@@ -24,8 +24,8 @@ from spdx_tools.spdx3.writer.json_ld.json_ld_writer import write_payload
     "--outfile",
     "-o",
     help="The file to write the converted document to (write a dash for output to stdout or omit for no conversion)."
-         "For now only a prototype serialization to json_ld is available. The provided file will therefore be extended "
-         "with `_jsonld.json`.",
+    "For now only a prototype serialization to json_ld is available. The provided file will therefore be extended "
+    "with `_jsonld.json`.",
 )
 @click.option(
     "--version",
