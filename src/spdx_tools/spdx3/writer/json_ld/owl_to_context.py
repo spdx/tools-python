715e8dc3d43380ef284d8e80cfdaf45a113ab9fa
Meret Behrens <meret.behrens@tngtech.com>
2023-05-02 13:01:35 +0200

    add documentation and update files
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx_tools/spdx3/writer/json_ld/owl_to_context.py
+++ src/spdx_tools/spdx3/writer/json_ld/owl_to_context.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 import json
+import os.path
 
 # current workflow: markdown files + spec_parser -> model.ttl -> convert to json_ld: SPDX_OWL.json ->
 # use the function below to generate context.json
@@ -38,27 +39,29 @@ PROPERTIES_WITH_ENUM_RANGE = [
 ]
 
 
-
-def convert_spdx_owl_to_jsonld_context():
-    with open("SPDX_OWL.json", "r") as infile:
+def convert_spdx_owl_to_jsonld_context(spdx_owl: str = "SPDX_OWL.json"):
+    with open(spdx_owl, "r") as infile:
         owl_dict = json.load(infile)
 
     context_dict = {
         "core": "https://spdx.org/rdf/Core/",
         "software": "https://spdx.org/rdf/Software/",
-        "xsd": "http://www.w3.org/2001/XMLSchema/",
+        "xsd": "http://www.w3.org/2001/XMLSchema#",
     }
 
     for node in owl_dict["@graph"]:
-        print(node)
-        node_type = node["@type"]
+        # print(node)
+        node_type = node.get("@type")
+        if not node_type:
+            # print(node)
+            continue
 
         if "owl:NamedIndividual" in node_type:
             continue
         elif node_type in ["owl:DatatypeProperty", "owl:ObjectProperty"]:
             name = node["@id"].split(":")[-1]
             if name in PROPERTIES_WITH_ENUM_RANGE:
-                context_dict[name]  = {
+                context_dict[name] = {
                     "@id": node["@id"],
                     "@type": "@vocab",
                     "@context": {"@vocab": node["rdfs:range"]["@id"] + "/"},
@@ -77,9 +80,9 @@ def convert_spdx_owl_to_jsonld_context():
         else:
             print(f"unknown node_type: {node_type}")
 
-    with open("context.json", "w") as infile:
+    with open(os.path.join(os.path.dirname(__file__), "context.json"), "w") as infile:
         json.dump(context_dict, infile)
 
 
 if __name__ == "__main__":
-    convert_spdx_owl_to_jsonld_context()
+    convert_spdx_owl_to_jsonld_context("SPDX_OWL.json")
