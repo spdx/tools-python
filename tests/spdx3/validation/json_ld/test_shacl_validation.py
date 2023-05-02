c784f5938582cbb6939972461077f1f50ba3e98e
Meret Behrens <meret.behrens@tngtech.com>
2023-05-02 13:01:35 +0200

    add documentation and update files
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- tests/spdx3/validation/json_ld/test_shacl_validation.py
+++ tests/spdx3/validation/json_ld/test_shacl_validation.py
@@ -1,13 +1,22 @@
 # SPDX-FileCopyrightText: 2023 spdx contributors
 #
 # SPDX-License-Identifier: Apache-2.0
+import os
+
+import pytest
+
 from spdx_tools.spdx3.validation.json_ld.shacl_validation import validate_against_shacl_from_file
 
 
+@pytest.mark.skip("Currently the validation against SHACL fails, refer to process.md and the known limitations.")
 def test_shacl_validation():
-    some_return = validate_against_shacl_from_file(
-        data_file="/home/armin/PycharmProjects/tools-python/tests/SPDX3_jsonld_test.json",
-        shacl_file="/home/armin/PycharmProjects/tools-python/src/spdx_tools/spdx3/writer/json_ld/model.ttl",
+    # insert path to example json ld
+    conforms, results_graph, results_text = validate_against_shacl_from_file(
+        data_file=os.path.join(os.path.dirname(__file__), "../../../SPDX3_jsonld_test.json"),
+        shacl_file=os.path.join(
+            os.path.dirname(__file__), "../../../../src/spdx_tools/spdx3/writer/json_ld/model.ttl"
+        ),
     )
-
-    print(some_return)
+    # results_graph.serialize("validation_result.rdf.xml", format="pretty-xml")
+    print(results_text)
+    assert conforms
