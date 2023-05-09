c8890f4215ca19ac0203325cf96b8c37e481f8e7
Armin Tänzer <armin.taenzer@tngtech.com>
2023-05-09 11:27:48 +0200

    [issue-426] Security profile: fix types
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- src/spdx_tools/spdx3/model/security/vex_vuln_assessment_relationship.py
+++ src/spdx_tools/spdx3/model/security/vex_vuln_assessment_relationship.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2023 spdx contributors
 #
 # SPDX-License-Identifier: Apache-2.0
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from typing import Optional
 
 from spdx_tools.common.typing.dataclass_with_properties import dataclass_with_properties
@@ -9,7 +9,7 @@ from spdx_tools.spdx3.model.security.vuln_assessment_relationship import VulnAss
 
 
 @dataclass_with_properties
-class VexVulnAssessmentRelationship(VulnAssessmentRelationship, ABC):
+class VexVulnAssessmentRelationship(VulnAssessmentRelationship):
     vex_version: Optional[str] = None
     status_notes: Optional[str] = None
 
--- src/spdx_tools/spdx3/model/security/vuln_assessment_relationship.py
+++ src/spdx_tools/spdx3/model/security/vuln_assessment_relationship.py
@@ -1,17 +1,17 @@
 # SPDX-FileCopyrightText: 2023 spdx contributors
 #
 # SPDX-License-Identifier: Apache-2.0
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from datetime import datetime
 from typing import Optional
 
 from spdx_tools.common.typing.dataclass_with_properties import dataclass_with_properties
-from spdx_tools.spdx3.model import Agent, Element, Relationship
+from spdx_tools.spdx3.model import Agent, Relationship
 
 
 @dataclass_with_properties
-class VulnAssessmentRelationship(Relationship, ABC):
-    assessed_element: Optional[Element] = None
+class VulnAssessmentRelationship(Relationship):
+    assessed_element: Optional[str] = None  # id of the element
     published_time: Optional[datetime] = None
     supplied_by: Optional[Agent] = None
     modified_time: Optional[datetime] = None
