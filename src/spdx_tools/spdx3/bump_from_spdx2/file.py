add35021a326af96b81e01107ab37370b4de7ce8
Meret Behrens <meret.behrens@tngtech.com>
2023-05-09 14:06:15 +0200

    add conversion for license expressions to package, file and snippet
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx_tools/spdx3/bump_from_spdx2/file.py
+++ src/spdx_tools/spdx3/bump_from_spdx2/file.py
@@ -2,15 +2,21 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 from spdx_tools.spdx3.bump_from_spdx2.checksum import bump_checksum
+from spdx_tools.spdx3.bump_from_spdx2.license_expression import bump_license_expression_or_none_or_no_assertion
 from spdx_tools.spdx3.bump_from_spdx2.message import print_missing_conversion
 from spdx_tools.spdx3.model import CreationInformation
 from spdx_tools.spdx3.model.software import File
 from spdx_tools.spdx3.payload import Payload
+from spdx_tools.spdx.model import SpdxNoAssertion
 from spdx_tools.spdx.model.file import File as Spdx2_File
 
 
 def bump_file(
-    spdx2_file: Spdx2_File, payload: Payload, creation_information: CreationInformation, document_namespace: str
+    spdx2_file: Spdx2_File,
+    payload: Payload,
+    creation_information: CreationInformation,
+    document_namespace: str,
+    extracted_licensing_info,
 ):
     spdx_id = "#".join([document_namespace, spdx2_file.spdx_id])
     integrity_methods = [bump_checksum(checksum) for checksum in spdx2_file.checksums]
@@ -19,14 +25,18 @@ def bump_file(
     print_missing_conversion(
         "file.file_type", 0, "different cardinalities, " "https://github.com/spdx/spdx-3-model/issues/82"
     )
+    license_concluded = bump_license_expression_or_none_or_no_assertion(
+        spdx2_file.license_concluded, extracted_licensing_info
+    )
+    copyright_text = None
+    if isinstance(spdx2_file.copyright_text, str):
+        copyright_text = spdx2_file.copyright_text
+    elif isinstance(spdx2_file.copyright_text, SpdxNoAssertion):
+        print_missing_conversion("package2.copyright_text", 0)
     print_missing_conversion(
-        "file.concluded_license, file.license_info_in_file, file.license_comment, file.copyright_text",
+        "file.notice, file.contributors, file.license_info_in_file, file.license_comment",
         0,
         "missing definition for license profile",
-    )
-
-    print_missing_conversion(
-        "file.notice, file.contributors, file.attribution_texts", 0, "missing definition for license profile"
     )
 
     payload.add_element(
@@ -36,5 +46,8 @@ def bump_file(
             name=spdx2_file.name,
             comment=spdx2_file.comment,
             verified_using=integrity_methods,
+            concluded_license=license_concluded,
+            copyright_text=copyright_text,
+            attribution_text=", ".join(spdx2_file.attribution_texts),
         )
     )
--- tests/spdx3/bump/test_file_bump.py
+++ tests/spdx3/bump/test_file_bump.py
@@ -5,6 +5,7 @@ from unittest import mock
 
 from spdx_tools.spdx3.bump_from_spdx2.file import bump_file
 from spdx_tools.spdx3.model import Hash, HashAlgorithm
+from spdx_tools.spdx3.model.licensing import ConjunctiveLicenseSet, ListedLicense
 from spdx_tools.spdx3.model.software import File
 from spdx_tools.spdx3.payload import Payload
 from spdx_tools.spdx.model.file import File as Spdx2_File
@@ -19,9 +20,14 @@ def test_bump_file(creation_information):
     integrity_method: Hash = Hash(HashAlgorithm.SHA1, "71c4025dd9897b364f3ebbb42c484ff43d00791c")
     expected_new_file_id = f"{document_namespace}#{spdx2_file.spdx_id}"
 
-    bump_file(spdx2_file, payload, creation_information, document_namespace)
+    bump_file(spdx2_file, payload, creation_information, document_namespace, None)
     file = payload.get_element(expected_new_file_id)
 
     assert isinstance(file, File)
     assert file.spdx_id == expected_new_file_id
     assert file.verified_using == [integrity_method]
+    assert file.concluded_license == ConjunctiveLicenseSet(
+        [ListedLicense("MIT", "MIT", ""), ListedLicense("GPL-2.0-only", "GPL-2.0-only", "")]
+    )
+    assert file.copyright_text == spdx2_file.copyright_text
+    assert file.attribution_text == spdx2_file.attribution_texts[0]
