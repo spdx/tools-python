f90e6a8f81c2d458d722d983b4c18f7f436146b6
Meret Behrens <meret.behrens@tngtech.com>
2023-05-09 14:06:15 +0200

    add conversion for license expressions to package, file and snippet
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/spdx_tools/spdx3/bump_from_spdx2/snippet.py
+++ src/spdx_tools/spdx3/bump_from_spdx2/snippet.py
@@ -1,26 +1,39 @@
 # SPDX-FileCopyrightText: 2023 spdx contributors
 #
 # SPDX-License-Identifier: Apache-2.0
+from typing import List
+
+from spdx_tools.spdx3.bump_from_spdx2.license_expression import bump_license_expression_or_none_or_no_assertion
 from spdx_tools.spdx3.bump_from_spdx2.message import print_missing_conversion
 from spdx_tools.spdx3.model import CreationInformation
 from spdx_tools.spdx3.model.software import Snippet
 from spdx_tools.spdx3.payload import Payload
+from spdx_tools.spdx.model import ExtractedLicensingInfo, SpdxNoAssertion
 from spdx_tools.spdx.model.snippet import Snippet as Spdx2_Snippet
 
 
 def bump_snippet(
-    spdx2_snippet: Spdx2_Snippet, payload: Payload, creation_information: CreationInformation, document_namespace: str
+    spdx2_snippet: Spdx2_Snippet,
+    payload: Payload,
+    creation_information: CreationInformation,
+    document_namespace: str,
+    extracted_licensing_info: List[ExtractedLicensingInfo],
 ):
     spdx_id = "#".join([document_namespace, spdx2_snippet.spdx_id])
     print_missing_conversion("snippet.file_spdx_id", 0, "https://github.com/spdx/spdx-3-model/issues/130")
+    concluded_license = bump_license_expression_or_none_or_no_assertion(
+        spdx2_snippet.license_concluded, extracted_licensing_info
+    )
+    copyright_text = None
+    if isinstance(spdx2_snippet.copyright_text, str):
+        copyright_text = spdx2_snippet.copyright_text
+    elif isinstance(spdx2_snippet.copyright_text, SpdxNoAssertion):
+        print_missing_conversion("package2.copyright_text", 0)
     print_missing_conversion(
-        "snippet.concluded_license, snippet.license_info_in_snippet, snippet.license_comment,"
-        "snippet.copyright_text",
+        "snippet.license_info_in_snippet, snippet.license_comment,",
         0,
         "missing definitions for license profile",
     )
-
-    print_missing_conversion("snippet.attribution_texts", 0, "missing definitions for license profile")
 
     payload.add_element(
         Snippet(
@@ -30,5 +43,8 @@ def bump_snippet(
             comment=spdx2_snippet.comment,
             byte_range=spdx2_snippet.byte_range,
             line_range=spdx2_snippet.line_range,
+            copyright_text=copyright_text,
+            attribution_text=", ".join(spdx2_snippet.attribution_texts),
+            concluded_license=concluded_license,
         )
     )
