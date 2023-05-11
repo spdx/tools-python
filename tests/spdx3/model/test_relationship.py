bdc8c0a070384aec8d5ba684872dc9b180c8efac
Armin Tänzer <armin.taenzer@tngtech.com>
2023-05-11 11:21:34 +0200

    [issue-432] refactor fixtures to deduplicate code from inherited properties
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- tests/spdx3/model/test_relationship.py
+++ tests/spdx3/model/test_relationship.py
@@ -6,30 +6,37 @@ from unittest import mock
 
 import pytest
 
+from spdx3.fixtures import relationship_fixture, ELEMENT_DICT, RELATIONSHIP_DICT
 from spdx_tools.spdx3.model import Relationship, RelationshipCompleteness, RelationshipType
 
 
 @mock.patch("spdx_tools.spdx3.model.CreationInformation", autospec=True)
 def test_correct_initialization(creation_information):
-    relationship = Relationship(
-        "SPDXRef-Relationship",
-        creation_information,
-        "spdx_id1",
-        RelationshipType.DESCRIBES,
-        ["spdx_id2", "spdx_id3"],
-        completeness=RelationshipCompleteness.NOASSERTION,
-        start_time=datetime(11, 11, 11),
-        end_time=datetime(12, 12, 12),
-    )
+    relationship = relationship_fixture()
 
-    assert relationship.spdx_id == "SPDXRef-Relationship"
-    assert relationship.creation_info == creation_information
-    assert relationship.from_element == "spdx_id1"
-    assert relationship.to == ["spdx_id2", "spdx_id3"]
-    assert relationship.relationship_type == RelationshipType.DESCRIBES
-    assert relationship.completeness == RelationshipCompleteness.NOASSERTION
-    assert relationship.start_time == datetime(11, 11, 11)
-    assert relationship.end_time == datetime(12, 12, 12)
+    local_dict = {}
+    local_dict.update(ELEMENT_DICT)
+    local_dict.update(RELATIONSHIP_DICT)
+    local_dict["spdx_id"] = "https://spdx.test/tools-python/relationship_fixture"
+
+    keys  = [
+        attribute
+        for attribute in dir(Relationship)
+        if isinstance(getattr(Relationship, attribute), property)
+    ]
+
+    for key in keys:
+        assert getattr(relationship, key) is not None
+        assert getattr(relationship, key) == local_dict[key]
+
+    # assert relationship.spdx_id == "SPDXRef-Relationship"
+    # assert relationship.creation_info == creation_information
+    # assert relationship.from_element == "spdx_id1"
+    # assert relationship.to == ["spdx_id2", "spdx_id3"]
+    # assert relationship.relationship_type == RelationshipType.DESCRIBES
+    # assert relationship.completeness == RelationshipCompleteness.NOASSERTION
+    # assert relationship.start_time == datetime(11, 11, 11)
+    # assert relationship.end_time == datetime(12, 12, 12)
 
 
 @mock.patch("spdx_tools.spdx3.model.CreationInformation", autospec=True)
