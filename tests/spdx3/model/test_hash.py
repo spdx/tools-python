fa385e7044a48cca3578053901a8a66cfffb1aab
Armin Tänzer <armin.taenzer@tngtech.com>
2023-05-16 11:51:35 +0200

    fix tests after typeguard update
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- tests/spdx3/model/test_hash.py
+++ tests/spdx3/model/test_hash.py
@@ -17,8 +17,6 @@ def test_invalid_initialization():
     with pytest.raises(TypeError) as err:
         Hash("SHA1", 345)
 
-    assert err.value.args[0] == [
-        'SetterError Hash: type of argument "algorithm" must be '
-        "spdx_tools.spdx3.model.hash.HashAlgorithm; got str instead: SHA1",
-        'SetterError Hash: type of argument "hash_value" must be str; got int ' "instead: 345",
-    ]
+    assert len(err.value.args[0]) == 2
+    for error in err.value.args[0]:
+        assert error.startswith("SetterError Hash:")
