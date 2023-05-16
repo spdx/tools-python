000a9dbbec7a77a934471a11f6e53e59ab3bbd77
Meret Behrens <meret.behrens@tngtech.com>
2023-05-16 08:59:46 +0200

    implement review comments
    - simplify the error message for values with multiple possible types
    - add value to the error message
    
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- tests/spdx/parser/jsonlikedict/test_error_message.py
+++ tests/spdx/parser/jsonlikedict/test_error_message.py
@@ -10,7 +10,12 @@ from spdx_tools.spdx.parser.jsonlikedict.package_parser import PackageParser
 # For all other classes we only check that a TypeError is raised if an incorrect type is specified.
 def test_error_message():
     package_parser = PackageParser()
-    package = {"SPDXID": "SPDXRef-Package", "downloadLocation": 5, "attributionTexts": ["text", 5, {"test": "data"}]}
+    package = {
+        "SPDXID": "SPDXRef-Package",
+        "downloadLocation": 5,
+        "attributionTexts": ["text", 5, {"test": "data"}],
+        "packageFileName": 10,
+    }
 
     with pytest.raises(SPDXParsingError) as err:
         package_parser.parse_package(package)
@@ -19,13 +24,13 @@ def test_error_message():
         err.value.get_messages(),
         [
             'Error while constructing Package: [\'SetterError Package: argument "name" '
-            "(None) is not an instance of str', 'SetterError Package: argument "
-            '"download_location" (int) did not match any element in the union:\\n  str: '
-            "is not an instance of str\\n  "
-            "spdx_tools.spdx.model.spdx_no_assertion.SpdxNoAssertion: is not an instance "
-            "of spdx_tools.spdx.model.spdx_no_assertion.SpdxNoAssertion\\n  "
-            "spdx_tools.spdx.model.spdx_none.SpdxNone: is not an instance of "
-            "spdx_tools.spdx.model.spdx_none.SpdxNone', 'SetterError Package: item 1 of "
-            'argument "attribution_texts" (list) is not an instance of str\']'
+            "(None) is not an instance of str: None', 'SetterError Package: argument "
+            '"download_location" (int) did not match any element in the union: '
+            "[\\'str\\', \\'spdx_tools.spdx.model.spdx_no_assertion.SpdxNoAssertion\\', "
+            "\\'spdx_tools.spdx.model.spdx_none.SpdxNone\\']: 5', 'SetterError Package: "
+            'argument "file_name" (int) did not match any element in the union: '
+            "[\\'str\\', \\'NoneType\\']: 10', 'SetterError Package: item 1 of argument "
+            "\"attribution_texts\" (list) is not an instance of str: [\\'text\\', 5, "
+            "{\\'test\\': \\'data\\'}]']"
         ],
     )
