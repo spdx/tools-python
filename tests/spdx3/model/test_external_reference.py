c029be0625df11e6667cb36ad24eb675833aca55
Armin Tänzer <armin.taenzer@tngtech.com>
2023-05-17 09:30:17 +0200

    [issue-432] use fixtures in initialization tests
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- tests/spdx3/model/test_external_reference.py
+++ tests/spdx3/model/test_external_reference.py
@@ -4,16 +4,20 @@
 import pytest
 
 from spdx_tools.spdx3.model import ExternalReference, ExternalReferenceType
+from tests.spdx3.fixtures import external_reference_fixture
+from tests.spdx3.model.model_test_utils import get_property_names
 
 
 def test_correct_initialization():
-    external_reference = ExternalReference(
-        ExternalReferenceType.SECURITY_ADVISORY, ["https://anyURI"], "MediaType", "This is a comment"
-    )
-    assert external_reference.external_reference_type == ExternalReferenceType.SECURITY_ADVISORY
-    assert external_reference.locator == ["https://anyURI"]
-    assert external_reference.content_type == "MediaType"
-    assert external_reference.comment == "This is a comment"
+    external_reference = external_reference_fixture()
+
+    for property_name in get_property_names(ExternalReference):
+        assert getattr(external_reference, property_name) is not None
+
+    assert external_reference.external_reference_type == ExternalReferenceType.OTHER
+    assert external_reference.locator == ["org.apache.tomcat:tomcat:9.0.0.M4"]
+    assert external_reference.content_type == "externalReferenceContentType"
+    assert external_reference.comment == "externalReferenceComment"
 
 
 def test_invalid_initialization():
--- tests/spdx3/model/test_hash.py
+++ tests/spdx3/model/test_hash.py
@@ -4,13 +4,19 @@
 import pytest
 
 from spdx_tools.spdx3.model import Hash, HashAlgorithm
+from tests.spdx3.fixtures import hash_fixture
+from tests.spdx3.model.model_test_utils import get_property_names
 
 
 def test_correct_initialization():
-    hash = Hash(algorithm=HashAlgorithm.SHA1, hash_value="value")
+    hash = hash_fixture()
+
+    for property_name in get_property_names(Hash):
+        assert getattr(hash, property_name) is not None
 
     assert hash.algorithm == HashAlgorithm.SHA1
-    assert hash.hash_value == "value"
+    assert hash.hash_value == "71c4025dd9897b364f3ebbb42c484ff43d00791c"
+    assert hash.comment == "hashComment"
 
 
 def test_invalid_initialization():
