d67b17e5b0f8f4acae44f37d2600969afc571541
Holger Frydrych <holger.frydrych@tngtech.com>
2023-05-23 17:04:17 +0200

    Add full shacl graph as ontology graph for validation
    Signed-off-by: Holger Frydrych <holger.frydrych@tngtech.com>

--- src/spdx_tools/spdx3/validation/json_ld/shacl_validation.py
+++ src/spdx_tools/spdx3/validation/json_ld/shacl_validation.py
@@ -19,12 +19,4 @@ def validate_against_shacl_from_file(
     with open(shacl_file) as file:
         shacl_graph.parse(file, format=shacl_format)
 
-    # we need to copy the named individuals created for our vocabulary types to
-    # an extra ontology graph since pySHACL ignores them in the provided shacl graph.
-    # if not provided in the ontology graph, validation fails due to those objects not being defined.
-    ont_graph = Graph()
-    named_individuals = shacl_graph.subjects(RDF.type, owlrl.OWL.NamedIndividual)
-    for named_individual in named_individuals:
-        ont_graph += shacl_graph.triples((named_individual, None, None))
-
-    return validate(data_graph=data_graph, shacl_graph=shacl_graph, ont_graph=ont_graph)
+    return validate(data_graph=data_graph, shacl_graph=shacl_graph, ont_graph=shacl_graph)
