08e9c03f009d9a5cb11cff9d7d1eba25d7d10b69
Holger Frydrych <holger.frydrych@tngtech.com>
2023-06-20 11:13:41 +0200

    Extract common validation, deduplication and conversion code for the writers
    Signed-off-by: Holger Frydrych <holger.frydrych@tngtech.com>

--- src/spdx_tools/spdx/writer/yaml/yaml_writer.py
+++ src/spdx_tools/spdx/writer/yaml/yaml_writer.py
@@ -2,13 +2,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 import yaml
-from beartype.typing import List, IO
+from beartype.typing import IO
 
-from spdx_tools.spdx.document_utils import create_document_without_duplicates
 from spdx_tools.spdx.jsonschema.document_converter import DocumentConverter
 from spdx_tools.spdx.model import Document
-from spdx_tools.spdx.validation.document_validator import validate_full_spdx_document
-from spdx_tools.spdx.validation.validation_message import ValidationMessage
+from spdx_tools.spdx.writer.write_utils import convert, validate_and_deduplicate
 
 
 def write_document_to_stream(
@@ -23,15 +21,8 @@ def write_document_to_stream(
     to False, validates the document before serialization. Unless a DocumentConverter instance is provided,
     a new one is created.
     """
-    if validate:
-        validation_messages: List[ValidationMessage] = validate_full_spdx_document(document)
-        if validation_messages:
-            raise ValueError(f"Document is not valid. The following errors were detected: {validation_messages}")
-    if drop_duplicates:
-        document = create_document_without_duplicates(document)
-    if converter is None:
-        converter = DocumentConverter()
-    document_dict = converter.convert(document)
+    document = validate_and_deduplicate(document, validate, drop_duplicates)
+    document_dict = convert(document, converter)
     yaml.safe_dump(document_dict, stream, indent=2)
 
 
