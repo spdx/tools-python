2e26b6b1dfa8bc5a7b3c8adbd64f704ed65eab74
Holger Frydrych <holger.frydrych@tngtech.com>
2023-06-23 12:11:18 +0200

    Add missing license headers in source files
    Signed-off-by: Holger Frydrych <holger.frydrych@tngtech.com>

--- src/spdx_tools/spdx/parser/parse_anything.py
+++ src/spdx_tools/spdx/parser/parse_anything.py
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: Apache-2.0
 # Copyright (c) spdx contributors
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
