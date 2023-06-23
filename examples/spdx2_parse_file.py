4acd23b552a3062c88e94ff0ec66a8901b4e4cb7
Holger Frydrych <holger.frydrych@tngtech.com>
2023-06-23 15:30:00 +0200

    Add example for parsing and using an existing SPDX2 document
    Signed-off-by: Holger Frydrych <holger.frydrych@tngtech.com>

---
+++ examples/spdx2_parse_file.py
@@ -0,0 +1,25 @@
+#  SPDX-FileCopyrightText: 2023 spdx contributors
+#
+#  SPDX-License-Identifier: Apache-2.0
+import logging
+from os import path
+
+from spdx_tools.spdx.model.document import Document
+from spdx_tools.spdx.parser.error import SPDXParsingError
+from spdx_tools.spdx.parser.parse_anything import parse_file
+
+# This example demonstrates how to parse an existing spdx file.
+
+# Provide a path to the input file
+input_path = path.join(path.dirname(__file__), "..", "tests", "spdx", "data", "SPDXLite.spdx")
+document: Document
+try:
+    # Try to parse the input file. If successful, returns a Document, otherwise raises an SPDXParsingError
+    document = parse_file(input_path)
+except SPDXParsingError:
+    logging.exception("Failed to parse spdx file")
+
+# We can now access attributes from the parsed document
+print(f"Parsed document name: {document.creation_info.name}")
+creators_as_str = ", ".join([creator.to_serialized_string() for creator in document.creation_info.creators])
+print(f"Created on {document.creation_info.created} by {creators_as_str}")
