283e3808e8432f4367119b6f2ff46a74884fdc0a
Holger Frydrych <holger.frydrych@tngtech.com>
2023-06-27 15:06:14 +0200

    Fix output file paths in pipeline run
    Signed-off-by: Holger Frydrych <holger.frydrych@tngtech.com>

--- tests/spdx/examples/test_examples.py
+++ tests/spdx/examples/test_examples.py
@@ -13,9 +13,8 @@ def cleanup_output_files():
 
     files_to_delete = ["spdx2_to_3.jsonld", "my_spdx_document.spdx.json", "converted_format.xml", "graph.png"]
     for file in files_to_delete:
-        output_file = os.path.join(os.path.dirname(__file__), file)
-        if os.path.exists(output_file):
-            os.remove(output_file)
+        if os.path.exists(file):
+            os.remove(file)
 
 
 def run_example(example_file: str):
@@ -30,25 +29,19 @@ def test_spdx2_parse_file():
 @pytest.mark.usefixtures("cleanup_output_files")
 def test_spdx2_convert_to_spdx3():
     run_example("spdx2_convert_to_spdx3.py")
-
-    output_file = os.path.join(os.path.dirname(__file__), "spdx2_to_3.jsonld")
-    assert os.path.exists(output_file)
+    assert os.path.exists("spdx2_to_3.jsonld")
 
 
 @pytest.mark.usefixtures("cleanup_output_files")
 def test_spdx2_document_from_scratch():
     run_example("spdx2_document_from_scratch.py")
-
-    output_file = os.path.join(os.path.dirname(__file__), "my_spdx_document.spdx.json")
-    assert os.path.exists(output_file)
+    assert os.path.exists("my_spdx_document.spdx.json")
 
 
 @pytest.mark.usefixtures("cleanup_output_files")
 def test_spdx2_convert_format():
     run_example("spdx2_convert_format.py")
-
-    output_file = os.path.join(os.path.dirname(__file__), "converted_format.xml")
-    assert os.path.exists(output_file)
+    assert os.path.exists("converted_format.xml")
 
 
 @pytest.mark.usefixtures("cleanup_output_files")
@@ -60,6 +53,4 @@ def test_spdx2_generate_graph():
         pytest.skip("Missing optional dependencies")
 
     run_example("spdx2_generate_graph.py")
-
-    output_file = os.path.join(os.path.dirname(__file__), "graph.png")
-    assert os.path.exists(output_file)
+    assert os.path.exists("graph.png")
