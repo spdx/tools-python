2a2bf70e9dcf39485723b32c3524f0a6aa81fd9c
Holger Frydrych <holger.frydrych@tngtech.com>
2023-07-11 16:52:59 +0200

    Define RDF namespaces for SPDX3
    Signed-off-by: Holger Frydrych <holger.frydrych@tngtech.com>

---
+++ src/spdx_tools/spdx3/rdfschema/namespace.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2023 spdx contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+from rdflib import Namespace
+
+AI_NS = Namespace("https://spdx.org/rdf/v3/AI/")
+BUILD_NS = Namespace("https://spdx.org/rdf/v3/Build/")
+CORE_NS = Namespace("https://spdx.org/rdf/v3/Core/")
+DATASET_NS = Namespace("https://spdx.org/rdf/v3/Dataset/")
+LICENSING_NS = Namespace("https://spdx.org/rdf/v3/Licensing/")
+SECURITY_NS = Namespace("https://spdx.org/rdf/v3/Security/")
+SOFTWARE_NS = Namespace("https://spdx.org/rdf/v3/Software/")
