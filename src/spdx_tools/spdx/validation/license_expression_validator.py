0eb0be8f9707cc8438a77817a7090a2b8d266fae
Armin Tänzer <armin.taenzer@tngtech.com>
2023-08-22 17:52:01 +0200

    [issue-744] implement validation of external license references
    Signed-off-by: Armin Tänzer <armin.taenzer@tngtech.com>

--- src/spdx_tools/spdx/validation/license_expression_validator.py
+++ src/spdx_tools/spdx/validation/license_expression_validator.py
@@ -7,6 +7,7 @@ from license_expression import ExpressionError, ExpressionParseError, LicenseExp
 
 from spdx_tools.common.spdx_licensing import spdx_licensing
 from spdx_tools.spdx.model import Document, SpdxNoAssertion, SpdxNone
+from spdx_tools.spdx.validation.spdx_id_validators import is_external_doc_ref_present_in_document
 from spdx_tools.spdx.validation.validation_message import SpdxElementType, ValidationContext, ValidationMessage
 
 
@@ -42,7 +43,36 @@ def validate_license_expression(
     license_ref_ids: List[str] = [license_ref.license_id for license_ref in document.extracted_licensing_info]
 
     for non_spdx_token in spdx_licensing.validate(license_expression).invalid_symbols:
-        if non_spdx_token not in license_ref_ids:
+        if ":" in non_spdx_token:
+            split_token: List[str] = non_spdx_token.split(":")
+            if len(split_token) != 2:
+                validation_messages.append(
+                    ValidationMessage(
+                        f"Too many colons in license reference: {non_spdx_token}. "
+                        "A license reference must only contain a single colon to "
+                        "separate an external document reference from the license reference.",
+                        context,
+                    )
+                )
+            else:
+                if not split_token[1].startswith("LicenseRef-"):
+                    validation_messages.append(
+                        ValidationMessage(
+                            f'A license reference must start with "LicenseRef-", but is: {split_token[1]} '
+                            f"in external license reference {non_spdx_token}.",
+                            context,
+                        )
+                    )
+                if not is_external_doc_ref_present_in_document(split_token[0], document):
+                    validation_messages.append(
+                        ValidationMessage(
+                            f'Did not find the external document reference "{split_token[0]}" in the SPDX document. '
+                            f"From the external license reference {non_spdx_token}.",
+                            context,
+                        )
+                    )
+
+        elif non_spdx_token not in license_ref_ids:
             validation_messages.append(
                 ValidationMessage(
                     f"Unrecognized license reference: {non_spdx_token}. license_expression must only use IDs from the "
