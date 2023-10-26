4513906dea5d9bc2e032431295236bdad21ad4e6
Maximilian Huber <maximilian.huber@tngtech.com>
2023-10-26 17:11:09 +0200

    wip
    Signed-off-by: Maximilian Huber <maximilian.huber@tngtech.com>

--- src/spdx_tools/spdx3/parser/json_ld/json_ld_parser.py
+++ src/spdx_tools/spdx3/parser/json_ld/json_ld_parser.py
@@ -9,21 +9,27 @@ from rdflib import Graph, URIRef, Literal
 from spdx_tools.spdx3.payload import Payload
 # from spdx_tools.spdx3.writer.json_ld.json_ld_converter import convert_payload_to_json_ld_list_of_elements
 
+def parse_from_file(file_name: str, encoding: str = "utf-8") -> Payload:
+    return JsonLDParser().parseFile(file_name, encoding)
+
+def parse_from_string(file_name: str) -> Payload:
+    return JsonLDParser().parseString(file_name)
+
+
 class JsonLDParser:
-    logger: Logger
+    # logger: Logger
 
-    def __init__(self):
-        self.logger = Logger()
-
-    def parseFileToGraph(self, file_name: str) -> Graph:
-        with open(file_name, "r") as file:
-            # read file to string
-            data = file.read()
-            return Graph().parse(data=data, format='json-ld')
+    # def __init__(self):
+    #     self.logger = Logger()
 
     def graphToPayload(self, graph: Graph) -> Payload:
         pass
 
-    def parseToPayload(self, file_name: str) -> Payload:
-        graph = self.parseFileToGraph(file_name)
+    def parseString(self, data: str) -> Payload:
+        graph = Graph().parse(data=data, format='json-ld')
         return self.graphToPayload(graph)
+
+    def parseFile(self, file_name: str, encoding: str = "utf-8") -> Payload:
+        with open(file_name, "r", encoding=encoding) as file:
+            data = file.read()
+            return self.parseStringToGraph(data)
