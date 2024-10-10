f8fecee1a38e23b0d88db3d1a218ccad36abdbd4
Arthit Suriyawongkul <arthit@gmail.com>
2024-10-11 05:00:15 +0700

    Update notes on context file
    Signed-off-by: Arthit Suriyawongkul <arthit@gmail.com>

--- src/spdx_tools/spdx3/writer/json_ld/json_ld_writer.py
+++ src/spdx_tools/spdx3/writer/json_ld/json_ld_writer.py
@@ -11,11 +11,10 @@ from spdx_tools.spdx3.writer.json_ld.json_ld_converter import convert_payload_to
 def write_payload(payload: Payload, file_name: str):
     element_list = convert_payload_to_json_ld_list_of_elements(payload)
 
-    # this will be obsolete as soon as the context is publicly available under some URI
-    # Use the context file from https://spdx.org/rdf/3.0.1/spdx-context.jsonld
+    # The code should be updated to use the context file from the public IRI
+    # such as https://spdx.org/rdf/3.0.1/spdx-context.jsonld
     with open(os.path.join(os.path.dirname(__file__), "context.json"), "r") as infile:
-        data = json.load(infile)
-        context = data.get("@context")
+        context = json.load(infile)
 
     complete_dict = {"@context": context, "@graph": element_list}
 
