7636b1b686f5b7541cfa7d67b27461c7b23e0a1e
Arthit Suriyawongkul <arthit@gmail.com>
2025-04-23 20:37:36 +0100

    Use import.resources instead of __file__
    Potentially fix #257 and allow zip-safe = true in pyproject.toml
    
    Signed-off-by: Arthit Suriyawongkul <arthit@gmail.com>

--- tests/spdx3/writer/json_ld/test_json_ld_writer.py
+++ tests/spdx3/writer/json_ld/test_json_ld_writer.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2023 spdx contributors
 #
 # SPDX-License-Identifier: Apache-2.0
-import os
+from importlib import resources
 
 from spdx_tools.spdx3.bump_from_spdx2.spdx_document import bump_spdx_document
 from spdx_tools.spdx3.payload import Payload
@@ -15,4 +15,5 @@ def test_json_writer():
     payload: Payload = bump_spdx_document(spdx2_document)
 
     # this currently generates an actual file to look at, this should be changed to a temp file later
-    write_payload(payload, os.path.join(os.path.dirname(__file__), "../../../SPDX3_jsonld_test"))
+    with resources.as_file(resources.files("tests.spdx3.writer.json_ld").joinpath("SPDX3_jsonld_test")) as output_file:
+        write_payload(payload, str(output_file))
