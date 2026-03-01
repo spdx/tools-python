01b9b7974e107f58b509301e558790754e8fcdb1
Zalan Blenessy <zalan.blenessy@volvocars.com>
2026-03-01 13:27:06 +0100

    Support microsecond timestamp resolution
    The dateTimeStamp XML standard type supports fractional
    second notation: https://www.w3.org/TR/xmlschema11-2/#dateTimeStamp
    
    Before this patch, the tool crashed when fractional timestamps were given
    in e.g. CreationInfo.
    
    Current implementation is limited to microsecond resolution.
    More fine-grained timestamps (nanosecond), will be silently
    truncated to microsecond resolution.
    
    Signed-off-by: Zalan Blenessy <zalan.blenessy@volvocars.com>

--- src/spdx_tools/spdx/datetime_conversions.py
+++ src/spdx_tools/spdx/datetime_conversions.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2022 spdx contributors
 #
 # SPDX-License-Identifier: Apache-2.0
+import re
 from datetime import datetime, timezone
 
 
@@ -8,8 +9,14 @@ def datetime_from_str(date_str: str) -> datetime:
     if not isinstance(date_str, str):
         raise TypeError(f"Could not convert str to datetime, invalid type: {type(date_str).__name__}")
 
-    date = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")  # raises ValueError if format does not match
-    return date
+    if "." not in date_str:
+        return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")  # raises ValueError if format does not match
+
+    # Based on the https://www.w3.org/TR/xmlschema11-2/#dateTimeStamp
+    # The secondFrag allows fractional second notation as well.
+    # normalize to micro seconds so that we can use %f with strptime
+    date_str = re.sub(r"\.(\d{1,6})\d*Z$", r".\1Z", date_str)
+    return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%fZ")  # raises ValueError if format does not match
 
 
 def datetime_to_iso_string(date: datetime) -> str:
