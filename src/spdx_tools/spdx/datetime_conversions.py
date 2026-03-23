c14c92f6642ca7e9372b47c7599dbbca144bcd14
zbleness <zalan.blenessy@volvocars.com>
2026-03-23 18:17:39 +0100

    Apply suggestions from code review
    Co-authored-by: Arthit Suriyawongkul <arthit@gmail.com>
    Signed-off-by: Zalan Blenessy <zalan.blenessy@volvocars.com>

--- src/spdx_tools/spdx/datetime_conversions.py
+++ src/spdx_tools/spdx/datetime_conversions.py
@@ -16,7 +16,13 @@ def datetime_from_str(date_str: str) -> datetime:
     # The secondFrag allows fractional second notation as well.
     # normalize to micro seconds so that we can use %f with strptime
     date_str = re.sub(r"\.(\d{1,6})\d*Z$", r".\1Z", date_str)
-    return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%fZ")  # raises ValueError if format does not match
+    warnings.warn(
+        "Invalid date format. Expected YYYY-MM-DDThh:mm:ssZ "
+        "Sub-second fractions have been discarded",
+        category=UserWarning,
+        stacklevel=2
+    )
+    return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")  # raises ValueError if format does not match
 
 
 def datetime_to_iso_string(date: datetime) -> str:
